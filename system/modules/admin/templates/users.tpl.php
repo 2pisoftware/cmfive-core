32b5e7855e46b116bedeb828febe0b847c4aeaf9
Adam Buckley <adam.buckley90@gmail.com>
2017-02-06 14:43:27 +1100

    Added tabbed layout to user list to separate internal/external users

--- system/modules/admin/templates/users.tpl.php
+++ system/modules/admin/templates/users.tpl.php
@@ -1,12 +1,16 @@
-<!--
-<div id="users-filter">
-    <form>
-        <table>
-            <tr><td>Show Only Active Users</td><td><input type="checkbox" name="show_active"/></td></tr>
-            <tr><td colspan="2"><input type="submit" value="Filter"/></td></tr>
-        </table>
-    </form>
-</div>
--->
 <?php echo Html::box($webroot."/admin/useradd/box","Add New User",true); ?>
-<?php echo $table; ?>
+<br/><br/>
+<div class='tabs'>
+	<div class='tab-head'>
+		<a class='active' href='#internal'>Internal</a>
+		<a href='#external'>External</a>
+	</div>
+	<div class='tab-body'>
+		<div id='internal'>
+			<?php echo $internal_table; ?>
+		</div>
+		<div id='external'>
+			<?php echo $external_table; ?>
+		</div>
+	</div>
+</div>
