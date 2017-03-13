4c2b3c6b54dcb6e76bba08fa6c423b13e7770caf
Adam Buckley <adam.buckley90@gmail.com>
2017-03-13 16:19:15 +1100

    Made preview template open in a new window to avoid using existing CSS
    Also removed the <hr/> inserted when the title is empty

--- system/modules/admin/templates/templates/edit.tpl.php
+++ system/modules/admin/templates/templates/edit.tpl.php
@@ -6,7 +6,7 @@
 		<a href="#details">Details</a>
 		<a href="#template">Template</a>
 		<a href="#test">Test Data</a>
-        <?php echo Html::box("/admin-templates/rendertemplate/". (!empty($id) ? $id : ""), "Test Output", false); ?>
+        <?php echo Html::a("/admin-templates/rendertemplate/". (!empty($id) ? $id : ""), "Test Output", null, null, null, '_blank'); ?>
 		<a href="#manual">Manual</a>
 	</div>
 	<div class="tab-body clearfix">
