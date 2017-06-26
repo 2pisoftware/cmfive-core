80e62680afabc9051c303469f5d4ab330352f0d6
Adam Buckley <adam.buckley90@gmail.com>
2017-06-27 09:29:20 +1000

    Reimplementing translations
    For some reason, we cannot merge files that have had new changes in with the old i18n branch. It might have something to do with the fact that this branch changes nearly every file.

--- system/modules/admin/actions/useredit.php
+++ system/modules/admin/actions/useredit.php
@@ -6,21 +6,23 @@
  * @param <type> $w
  */
 function useredit_GET(Web &$w) {
-    $p = $w->pathMatch("id", "box");
-    $user = $w->Auth->getObject("User", $p["id"]);
-    if ($user) {
-        $w->Admin->navigation($w, "Administration - Edit User - " . $user->login);
-    } else {
-        if (!$p['box']) {
-            $w->error("User " . $w->ctx("id") . " does not exist.", "/admin/users");
-        }
-    }
-    $w->ctx("user", $user);
+	$p = $w->pathMatch("id", "box");
+	$user = $w->Auth->getObject("User", $p["id"]);
+	$w->ctx('availableLocales', $user->getAvailableLanguages());
 
-    // no layout if displayed in a box
-    if ($p['box']) {
-        $w->setLayout(null);
-    }
+	if ($user) {
+		$w->Admin->navigation($w, "Administration - Edit User - " . $user->login);
+	} else {
+		if (!$p['box']) {
+			$w->error("User " . $w->ctx("id") . " does not exist.", "/admin/users");
+		}
+	}
+	$w->ctx("user", $user);
+
+	// no layout if displayed in a box
+	if ($p['box']) {
+		$w->setLayout(null);
+	}
 }
 
 /**
@@ -29,39 +31,39 @@ function useredit_GET(Web &$w) {
  * @param <type> $w
  */
 function useredit_POST(Web &$w) {
-    $w->pathMatch("id");
-    $errors = $w->validate(array(
-        array("login", ".+", "Login is mandatory")
-    ));
-    if ($_REQUEST['password'] && ($_REQUEST['password'] != $_REQUEST['password2'])) {
-        $error[] = "Passwords don't match";
-    }
-    $user = $w->Auth->getObject("User", $w->ctx('id'));
-    if (!$user) {
-        $errors[] = "User does not exist";
-    }
-    if (sizeof($errors) != 0) {
-        $w->error(implode("<br/>\n", $errors), "/admin/useredit/" . $w->ctx("id"));
-    }
-    $user->login = $_REQUEST['login'];
+	$w->pathMatch("id");
+	$errors = $w->validate(array(
+		array("login", ".+", "Login is mandatory"),
+	));
+	if ($_REQUEST['password'] && ($_REQUEST['password'] != $_REQUEST['password2'])) {
+		$error[] = "Passwords don't match";
+	}
+	$user = $w->Auth->getObject("User", $w->ctx('id'));
+	if (!$user) {
+		$errors[] = "User does not exist";
+	}
+	if (sizeof($errors) != 0) {
+		$w->error(implode("<br/>\n", $errors), "/admin/useredit/" . $w->ctx("id"));
+	}
+	$user->login = $_REQUEST['login'];
 
-    $user->fill($_REQUEST);
-    if ($_REQUEST['password']) {
-        $user->setPassword($_REQUEST['password']);
-    } else {
-        $user->password = null;
-    }
-    $user->is_admin = isset($_REQUEST['is_admin']) ? 1 : 0;
-    $user->is_active = isset($_REQUEST['is_active']) ? 1 : 0;
-    $user->update();
+	$user->fill($_REQUEST);
+	if ($_REQUEST['password']) {
+		$user->setPassword($_REQUEST['password']);
+	} else {
+		$user->password = null;
+	}
+	$user->is_admin = isset($_REQUEST['is_admin']) ? 1 : 0;
+	$user->is_active = isset($_REQUEST['is_active']) ? 1 : 0;
+	$user->update();
 
-    $contact = $user->getContact();
-    if ($contact) {
-        $contact->fill($_REQUEST);
-        $contact->private_to_user_id = null;
-        $contact->update();
-    }
-    $w->callHook("admin", "account_changed", $user);
+	$contact = $user->getContact();
+	if ($contact) {
+		$contact->fill($_REQUEST);
+		$contact->private_to_user_id = null;
+		$contact->update();
+	}
+	$w->callHook("admin", "account_changed", $user);
 
-    $w->msg("<div id='saved_record_id' data-id='".$user->id."' >User " . $user->login . " updated.</div>", "/admin/users");
+	$w->msg("<div id='saved_record_id' data-id='" . $user->id . "' >User " . $user->login . " updated.</div>", "/admin/users");
 }
