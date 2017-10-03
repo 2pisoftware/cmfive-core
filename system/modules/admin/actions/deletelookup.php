9dc834e80fd5e9161b3dde0e1ae5e080dbf1c813
Jack Robbers <jack@2pisoftware.com>
2017-10-03 21:00:58 +1100

    Prevent user from deleting title lookups when they are in use

--- system/modules/admin/actions/deletelookup.php
+++ system/modules/admin/actions/deletelookup.php
@@ -1,16 +1,25 @@
 <?php
 function deletelookup_ALL(Web &$w) {
 	$p = $w->pathMatch("id","type");
-		
+	$return_url = "/admin/lookup/?type=".$p['type'];
 	$lookup = $w->Admin->getLookupbyId($p['id']);
-		
+	
 	if ($lookup) {
+		$contacts_used_by_lookup = $w->Admin->getObjects('contact', ['title_lookup_id' => $p['id']]);
+		if (!empty($contacts_used_by_lookup)) {
+			$list_of_contacts = "";
+			foreach ($contacts_used_by_lookup as $contact) {
+				$list_of_contacts .= $contact->getFullName() . ', ';
+			}
+			$list_of_contacts = substr($list_of_contacts, 0, -2);
+			$w->msg("Cannot delete title as it is used as a title for the contacts: " . $list_of_contacts, $return_url);
+		}
 		$arritem['is_deleted'] = 1;
 		$lookup->fill($arritem);
 		$lookup->update();
-		$w->msg("Lookup Item deleted","/admin/lookup/?type=".$p['type']);
+		$w->msg("Lookup Item deleted", $return_url);
 	}
 	else {
-		$w->msg("Lookup Item not found?","/admin/lookup/?type=".$p['type']);
+		$w->msg("Lookup Item not found?", $return_url);
 	}
 }
