760c520c457a4ab344fcf8abef623f3c7ae6a580
Adam Buckley <adam.buckley90@gmail.com>
2017-11-02 13:20:09 +1100

    Added CurlService to main, added thumbnail cache function to attachments

---
+++ system/modules/main/models/CurlService.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * CURL helper static class
+ * 
+ * @author <adam@2pisoftware.com>
+ */
+class CurlService extends DbService {
+
+	/**
+	 * Creates and runs a get request and passes the response to a callback
+	 * 
+	 * @param string $url
+	 * @param array $data
+	 * @param Function $callback
+	 */
+	public static function getRequest($url, Array $data = [], $callback = null) {
+		$handle = curl_init();
+		
+		curl_setopt($handle, CURLOPT_URL, $url . (!empty($data) && is_array($data) ? '?' . http_build_query($data) : ''));
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+		
+		$output = curl_exec($handle);
+		$error = curl_error($handle);
+		
+		curl_close($handle);
+		
+		if (is_callable($callback)) {
+			$callback($output, $error);
+		}
+
+		return $output;
+	}
+	
+	/**
+	 * Creates and runs a post request and passes the response to a callback
+	 * 
+	 * @param string $url
+	 * @param array $data
+	 * @param Function $callback
+	 */
+	public static function postRequest($url, Array $data = [], $callback = null) {
+		$handle = curl_init();
+		
+		curl_setopt($handle, CURLOPT_URL, $url);
+		curl_setopt($handle, CURLOPT_POST, 1);
+		curl_setopt($handle, CURLOPT_POSTFIELDS, http_build_query($data));
+
+		// receive server response
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+		$output = curl_exec($handle);
+		$error = curl_error($handle);
+
+		curl_close($handle);
+		
+		if (is_callable($callback)) {
+			$callback($output, $error);
+		}
+
+		return $output;
+	}
+}
