a6fd56ca0e9ec520a133d961bc8ff5b6b001f139
isaac <iceaxe.liehne@gmail.com>
2018-03-27 12:47:07 +1100

    added export buttons to application pages

--- system/modules/form/templates/application/show.tpl.php
+++ system/modules/form/templates/application/show.tpl.php
@@ -16,7 +16,8 @@
 	<div class='small-12 medium-3 columns'>
 		<div class='row'>
 			<div class='small-6 columns'>
-				<?php echo Html::b('/form-application/edit/' . $application->id, 'Edit', null, null, false, "button expand"); ?>
+				<?php echo Html::b('/form-application/edit/' . $application->id, 'Edit', null, null, false, "button expand"); ?> </br>
+				<?php echo Html::b('/form-application/export/'.$application->id, 'Export', null, null, false, "button expand"); ?>
 			</div>
 			<div class='small-6 columns'>
 				<?php echo Html::b('/form-application/delete/' . $application->id, 'Delete', 'Are you sure you want to delete this application? All references to already entered data will be lost!', null, false, "warning expand"); ?>
