0650195f831752cae87f18a931418544ef38e4dc
isaac <iceaxe.liehne@gmail.com>
2018-04-05 16:32:49 +1000

    add Form views in database with cron

---
+++ system/modules/form/actions/cron/updateformview.php
@@ -0,0 +1,23 @@
+<?php
+
+function updateformview_ALL(Web $w) {
+
+	$forms = $w->Form->getForms();
+
+	if (!empty($forms)) {
+		foreach ($forms as $form) {
+			$settings = 'SET SESSION group_concat_max_len = 1000000';
+			$set = $w->db->query($settings)->execute();
+
+			$form_id = $form->id;
+			$query = 'SELECT CONCAT("SELECT form_instance.id, ", GROUP_CONCAT("t_", form_field.technical_name, ".value AS ", form_field.technical_name), " FROM form LEFT JOIN form_instance ON form_instance.form_id = form.id ", GROUP_CONCAT("LEFT JOIN form_value AS t_", form_field.technical_name, " ON (form_instance.id = t_", form_field.technical_name, ".form_instance_id AND t_", form_field.technical_name, ".form_field_id = ", QUOTE(form_field.id), ")" SEPARATOR " "  ), " WHERE form.id = ' . $form_id .' ") as "view_query" FROM form_field WHERE form_id = ' . $form_id;
+
+			$result = $w->db->query($query)->fetch(PDO::FETCH_ASSOC);
+
+			$view_name = str_replace(' ', '_', $form->title) . '_view';
+			$query2 = 'CREATE OR REPLACE VIEW ' . $view_name . ' AS ' . $result['view_query'];
+
+			$view = $w->db->query($query2)->execute();
+		}
+	}
+}
