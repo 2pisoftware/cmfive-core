a7ad63f1993de793410b774913f65933dc9aa9c7
Adam Buckley <adam@2pisoftware.com>
2018-04-13 16:36:07 +1000

    Added additional prop definition (like "defer") to scripts

--- system/classes/components/CmfiveScriptComponent.php
+++ system/classes/components/CmfiveScriptComponent.php
@@ -2,11 +2,17 @@
 
 class CmfiveScriptComponent extends CmfiveComponent {
 
-	public $tag = 'script';
-	public $has_closing_tag = true;
+    public $tag = 'script';
+    public $has_closing_tag = true;
 
-	public function __construct($path) {
+    public function __construct($path, Array $props = []) {
 		$this->src = $path;
-	}
+
+		if (!empty($props)) {
+		    foreach($props as $key => $value) {
+				$this->$key = $value;
+			}
+		}
+    }
 
 }
