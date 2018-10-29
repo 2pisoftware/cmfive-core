67ae5af2a8664520bebddcb9fca7b430ff450de0
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2018-10-29 13:00:07 +1100

    ""

--- system/templates/vue-components/html/html-tabs.vue.js
+++ system/templates/vue-components/html/html-tabs.vue.js
@@ -3,29 +3,31 @@
  */
 
 Vue.component('html-tabs', {
-	data: function() {
-		return {
-			tabs: []
-		};
-	},
-	methods: {
-		selectTab: function(tab) {
-			for(var i in this.tabs) {
-				this.tabs[i].isActive = (tab.title === this.tabs[i].title);
-			}
-		}
-	},
-	template:   '<div class="tabs"> \
-					<ul> \
-						<li v-for="tab in tabs" :class="{\'active_tab\': tab.isActive}" v-on:click="selectTab(tab)"> \
-							<i v-if="tab.icon" class="fas tab-icon" :class="tab.icon"></i> <a :href="tab.href">{{ tab.title }}</a> \
-						</li> \
-					</ul> \
-					<div class="tab-content"> \
-						<slot></slot> \
-					</div> \
-				</div>',
-	created: function() {
-		this.tabs = this.$children;
-	}
+    data: function() {
+        return {
+            tabs: []
+        };
+    },
+    methods: {
+        selectTab: function(tab, event) {
+            event.preventDefault()
+            
+            for(var i in this.tabs) {
+                this.tabs[i].isActive = (tab.title === this.tabs[i].title);
+            }
+        }
+    },
+    template:   '<div class="tabs"> \
+                    <ul> \
+                        <li v-for="tab in tabs" :class="{\'active_tab\': tab.isActive}" v-on:click="selectTab(tab, $event)"> \
+                            <i v-if="tab.icon" class="fas tab-icon" :class="tab.icon"></i> <a :href="tab.href">{{ tab.title }}</a> \
+                        </li> \
+                    </ul> \
+                    <div class="tab-content"> \
+                        <slot></slot> \
+                    </div> \
+                </div>',
+    created: function() {
+        this.tabs = this.$children;
+    }
 });
