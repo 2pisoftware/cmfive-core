1b39f5af1a1e27a23ff50da69d5bbc87e97f1b36
Jade Short <jadeshort4@gmail.com>
2019-01-21 11:22:33 +1100

    (Barely) Styled Pre Message Page
    Centered Text and buttons. Stronger Heading.

--- system/modules/admin/templates/migration/migrationmessage.tpl.php
+++ system/modules/admin/templates/migration/migrationmessage.tpl.php
@@ -1,11 +1,13 @@
 <?php
 if (!empty($migration_filename) && !empty($migration_module) && !empty($migration_preText))
 {
-    echo "<h4><strong>$migration_class</strong></h4>";
+    echo "<center>";
+    echo "<h1><strong>$migration_class</strong></h1>";
     echo $migration_preText;
     echo "<br><br>";
     echo Html::b("/admin-migration#batch", "Cancel Migration", "Are you sure you would like to cancel?", null, null);
     echo Html::b("/admin-migration/run/". $migration_module . "/" . $migration_filename . "?ignoremessages=" . "true", "Continue Migration", "Are you sure you would like to continue?", null, null);
+    echo "</center>";
 } else {
     $w->error("Not all migration fields specified", "/admin-migration#batch");
 } ?>
