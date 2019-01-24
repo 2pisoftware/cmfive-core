a6b1ffab91013fb3e046c5d569cb6d3f03230037
Jade Short <jadeshort4@gmail.com>
2019-01-24 14:51:33 +1100

    Added Migration Reindex functionality

--- system/modules/search/install/migrations/20190115145342-SearchMySQLStopwords.php
+++ system/modules/search/install/migrations/20190115145342-SearchMySQLStopwords.php
@@ -4,20 +4,21 @@ class SearchMySQLStopwords extends CmfiveMigration {
 
 	public function up() {
 		// UP
+		$w->Search->reindexAll();
 	}
 
 	public function down() {
 		// DOWN
 	}
 
-	public function postText()
+	public function preText()
 	{
-		return "<div style='color:red;'>REQUIRED: </div> Go to \modules\search\install\customStopwordSetup.txt for instructions on how to disable the default MySQL stopwords.";
+		return "<div style='color:red;'>REQUIRED: </div> Go to \modules\search\install\customStopwordSetup.txt for instructions on how to disable the default MySQL stopwords before running this migration.";
 	}
 
 	public function description()
 	{
-		return "Blank Migration that gives instructions on how disable MySQL default stopwords.";
+		return "Migration that gives instructions on disabling default mysql stopwords. Also reindexes object_index table to account of the change in the stopwords list.";
 	}
 
 }
