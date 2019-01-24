ab2b7fcc746f7bb99eaaeb13c11bd99422e88237
Jade Short <jadeshort4@gmail.com>
2019-01-25 09:20:22 +1100

    Cleanup

--- system/modules/search/templates/reindexpage.tpl.php
+++ system/modules/search/templates/reindexpage.tpl.php
@@ -1,2 +1,5 @@
+<p> Use this button after altering Cmfive stopwords in the config file. </p>
 <?php 
- echo Html::b('/search/reindexall', 'Reindex All', 'Are you sure you would like to reindex all objects?');
+ echo Html::b('/search/reindexall', 'Reindex All Search Objects', 'Are you sure you would like to reindex all objects?');
+ ?>
+
