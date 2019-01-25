e9cf3b516d040dba98c592b453b3339e961b4e99
Jade Short <jadeshort4@gmail.com>
2019-01-25 13:30:23 +1100

    Stopword Table Migration

--- system/modules/search/install/migrations/20190115145342-SearchMySQLStopwords.php
+++ system/modules/search/install/migrations/20190115145342-SearchMySQLStopwords.php
@@ -4,21 +4,33 @@ class SearchMySQLStopwords extends CmfiveMigration {
 
 	public function up() {
 		// UP
+
+		if (!$this->hasTable('my_stopwords')) {
+			$this->table('my_stopwords', [
+						'id' => false,
+					])
+					->addColumn('value', 'string')
+					->create();
+				$query = "INSERT INTO my_stopwords(value) VALUE ('dgiwdgwidwdiv2');";
+				$this->w->db->query($query)->execute();
+		}
+
 		$this->w->Search->reindexAll();
 	}
 
 	public function down() {
 		// DOWN
+		$this->hasTable('my_stopwords') ? $this->dropTable('my_stopwords') : null;
 	}
 
-	public function preText()
+	public function postText()
 	{
-		return "<div style='color:red;'>REQUIRED: </div> Go to \modules\search\install\customStopwordSetup.txt for instructions on how to disable the default MySQL stopwords before running this migration.";
+		return "<div style='color:red;'>REQUIRED: </div> Go to \modules\search\install\customStopwordSetup.txt for instructions on how to disable the default MySQL stopwords.";
 	}
 
 	public function description()
 	{
-		return "Migration that gives instructions on disabling default mysql stopwords. Also reindexes object_index table to account of the change in the stopwords list.";
+		return "Migration that creates the stopwords table to overide the mysql defaults. Also reindexes object_index table to account of the change in the stopwords list.";
 	}
 
 }
