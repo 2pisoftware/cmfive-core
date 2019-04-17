bdcd59d32bf55a5680b9ffbab1680c786d34f0f4
Jade Short <jadeshort4@gmail.com>
2019-04-17 13:08:15 +1000

    Page security for reindex page

--- system/modules/search/actions/reindexpage.php
+++ system/modules/search/actions/reindexpage.php
@@ -1,6 +1,9 @@
 <?php //display all publishers games and website url
 function reindexpage_ALL(Web $w) {
-
+    if (!$w->Auth->user()->is_admin) {
+        $w->error("Access Restricted");
+    }
+    
     $w->ctx("title", "Search Admin");
 
 }
