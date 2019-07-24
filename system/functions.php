cb480dd8cf95fb66276c6f19b4aefdd3c3135c88
unknown <derek@tripleacs.com>
2019-07-24 12:31:56 +1000

    AES/SSL Remove configIV references, add logging

--- system/functions.php
+++ system/functions.php
@@ -735,7 +735,9 @@ function SystemSSLencrypt($text) {
 	$encryption_iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($ssl_method));
 	if (empty($encryption_key) || empty($encryption_iv)) {
 		// raise exception
-		throw new Exception('Cannot encrypt/decrypt without system key.');
+		$err = 'Cannot encrypt without system key and IV.';
+		$this->w->Log->error($err);
+		throw new Exception($err);
 	} else {
 		$ssl = openssl_encrypt($text, $ssl_method, $encryption_key, 0, $encryption_iv);	
 		$encryption_iv = bin2hex($encryption_iv);
@@ -760,7 +762,9 @@ function SystemSSLdecrypt($text) {
 	$encryption_iv = hex2bin(array_pop($text));
 	if (empty($encryption_key) || empty($encryption_iv)) {
 		// raise exception
-		throw new Exception('Cannot encrypt/decrypt without system key.');
+		$err = 'Cannot decrypt without system key and IV.';
+		$this->w->Log->error($err);
+		throw new Exception($err);
 		} else {
 		$text = array_pop($text);
 		return openssl_decrypt($text, $ssl_method, $encryption_key, 0, $encryption_iv);
