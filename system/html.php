fdd23323531bdd3d541ec7d57c3ae207db2b45ec
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2019-08-28 14:38:23 +1000

    Auto stash before merge of "feature/LocalS3FileCache" and "develop"

--- system/html.php
+++ system/html.php
@@ -1503,6 +1503,6 @@ UPLOAD;
             $type = "info";
         }
 
-        return "<div data-alert class=\"alert-box " . $type . "\">" . $msg . "<a href=\"#\" class=\"close\">&times;</a></div>";
+        return "<div data-alert class='alert-box {$type}'>{$msg}<a href='#' class='close'>&times;</a></div>";
     }
 }
