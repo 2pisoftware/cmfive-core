b222af282e26b01a3ccfb6490feaaae67e1f758b
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2019-09-11 08:41:29 +1000

    Added property_exists check to is_deleted and formatted document

--- system/modules/favorite/partials/actions/display_favorites.php
+++ system/modules/favorite/partials/actions/display_favorites.php
@@ -1,33 +1,31 @@
-<?php namespace System\Modules\Favorite;
+<?php
 
-// Replacement function for listFavourite!
+namespace System\Modules\Favorite;
 
-function display_favorites(\Web $w) {	
-	$results = $w->Favorite->getFavoritesForUser($w->Auth->user()->id);
-	$categorisedFavorites = array();
-	if (!empty($results)) {
-		foreach ($results as $k => $favorite) {
-			if (!array_key_exists($favorite->object_class, $categorisedFavorites)) {
-				$categorisedFavorites[$favorite->object_class] = array();
-			}
+function display_favorites(\Web $w)
+{
+    $results = $w->Favorite->getFavoritesForUser($w->Auth->user()->id);
+    $categorisedFavorites = array();
+    if (!empty($results)) {
+        foreach ($results as $k => $favorite) {
+            if (!array_key_exists($favorite->object_class, $categorisedFavorites)) {
+                $categorisedFavorites[$favorite->object_class] = array();
+            }
 
-			$object = $favorite->getLinkedObject();
-			if (!empty($object) && !$object->is_deleted)  {
-				$categorisedFavorites[$favorite->object_class][] = $object;
-			}
-		}
-		
-		// Sort data
-		ksort($categorisedFavorites);
-		foreach($categorisedFavorites as &$categorisedFavorite) {
-			usort($categorisedFavorite, function($a, $b) {
-				return strcmp($a, $b);
-			});
-		}
-	}
-	
-	
-	
-	
-	$w->ctx('categorisedFavorites', $categorisedFavorites);
+            $object = $favorite->getLinkedObject();
+            if (!empty($object) && property_exists($object, "is_deleted") && !$object->is_deleted) {
+                $categorisedFavorites[$favorite->object_class][] = $object;
+            }
+        }
+
+        // Sort data
+        ksort($categorisedFavorites);
+        foreach ($categorisedFavorites as &$categorisedFavorite) {
+            usort($categorisedFavorite, function ($a, $b) {
+                return strcmp($a, $b);
+            });
+        }
+    }
+
+    $w->ctx('categorisedFavorites', $categorisedFavorites);
 }
