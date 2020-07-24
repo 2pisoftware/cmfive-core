cde60b766adcc3f84f03f360895cac1e0a72399e
Alice <alice.hutley@gmail.com>
2020-07-24 13:51:34 +1000

    Adding index action to list reports

--- system/modules/insights/models/InsightsService.php
+++ system/modules/insights/models/InsightsService.php
@@ -3,3 +3,7 @@
 class InsightsService extends DbService {
 
 }
+// returns all insights reports instances
+public function GetAllReports() {
+    return $this->GetObjects('InsightsReports',['is_deleted'=>0]);
+}
