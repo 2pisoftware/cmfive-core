8947854c3630882a8a91b93724aff5b76a45c035
Alice <alice.hutley@gmail.com>
2020-07-31 13:16:23 +1000

    adding functions

--- system/modules/insights/models/InsightsService.php
+++ system/modules/insights/models/InsightsService.php
@@ -1,10 +1,26 @@
 <?php
 
-class InsightsService extends DbService
+class InsightsService extends Insight
 {
     // returns all insights reports instances
-    public function GetAllInsights()
+    public function GetAllInsights($module)
     {
-        
+        $availableInsights = [];
+
+		// Read insights directory for all insights
+		if ($module === 'insights') {
+			foreach($this->w->modules() as $insight) {
+				$availableInsights += $this->getInsightsForModule($insight);
+			}
+		} else {
+			$availableInsights = $this->getInsightsForModule($module);
+		}
+		
+		return $availableInsights;
+    }
+
+    public function getInsightsForModule($insight)
+    {
+
     }
 }
