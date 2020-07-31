75f7b1b23cef4fd1c1e9b80235d76bb78b3b8dc5
Alice <alice.hutley@gmail.com>
2020-07-31 13:15:46 +1000

    Overriding base variables

--- system/modules/insights/models/TestInsight.php
+++ system/modules/insights/models/TestInsight.php
@@ -3,5 +3,14 @@
 
 class TestInsight extends Insight
 {
-    
+    public $name="Test Insight";
+    public $description="Test model for insights";
+
+    public function getFilters()
+    {
+    }
+
+    public function run()
+    {
+    }
 }
