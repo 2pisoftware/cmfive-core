c8765308e7e5e009d47128d3087fea8741570780
Alice <alice.hutley@gmail.com>
2020-07-31 13:38:28 +1000

    Adding functions

--- system/modules/insights/models/InsightsService.php
+++ system/modules/insights/models/InsightsService.php
@@ -21,6 +21,37 @@ class InsightsService extends Insight
 
     public function getInsightsForModule($insight)
     {
+        $availableInsights = [];
 
+        // Check insights folder
+        $module_path = PROJECT_MODULE_DIRECTORY . DS . $insight . DS . MIGRATION_DIRECTORY;
+        $system_module_path = SYSTEM_MODULE_DIRECTORY . DS . $insight . DS . MIGRATION_DIRECTORY;
+
+        $insight_paths = [$module_path, $system_module_path];
+        if (empty($availableInsights[$insight])) {
+            $availableInsights[$insight] = [];
+        }
+
+        foreach ($insight_paths as $insight_path) {
+            if (is_dir(ROOT_PATH . DS . $insight_path)) {
+                foreach (scandir(ROOT_PATH . DS . $insight_path) as $file) {
+                    if (!is_dir($file) && $file{
+                        0} !== '.') {
+                        $classname = explode('.',$file));
+                          //Create instance of class
+                          $insightspath = $insight_path . DS . $file;
+                          if (file_exists(ROOT_PATH . DS . $insightspath)) {
+                              include_once ROOT_PATH . DS . $insightspath;
+
+                              $insight_class = preg_replace('/.php$/', '', $insight_class);
+                              if (class_exists($insight_class)) {
+                                  $insight = (new $insight_class(1))->setWeb($this->w);
+                              }
+                            }
+                        }
+                    }
+                }
+            }
+                              return $availableInsights;
     }
 }
