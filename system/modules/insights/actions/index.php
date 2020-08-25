ee81f975140e3351a64ddce1760eec8e6eed7a6f
Alice <alice.hutley@gmail.com>
2020-08-25 14:23:01 +1000

    find insights module and correct classes

--- system/modules/insights/actions/index.php
+++ system/modules/insights/actions/index.php
@@ -13,22 +13,22 @@ function index_ALL(Web $w)
 
 
     // access service functions using the Web $w object and the module name
-    $insights = $w->Insights->getAllInsights('all');
-
+    $insights = InsightService::getInstance($w)->getAllInsights('all');
+    var_dump($insights);
     // build the table array adding the headers and the row data
     $table = [];
     $tableHeaders = ['Name'];
-    if (!empty($insights)) {  // only loop if we have one or more reports
-        foreach ($insights as $insight) { // loop through each report
-            $row = [];
-            // add values to the row in the same order as the table headers
-            $row[] = $insight->name;
-            // the actions column is used to hold buttons that link to actions per report. Note the report id is added to the href on these buttons.
-            $actions = [];
-            $row[] = implode('', $actions);
-            $table[] = $row;
-        }
-    }
+    // if (!empty($insights)) {  // only loop if we have one or more reports
+    //     foreach ($insights as $insight) { // loop through each report
+    //         $row = [];
+    //         // add values to the row in the same order as the table headers
+    //         $row[] = $insight->name;
+    //         // the actions column is used to hold buttons that link to actions per report. Note the report id is added to the href on these buttons.
+    //         $actions = [];
+    //         $row[] = implode('', $actions);
+    //         $table[] = $row;
+    //     }
+    //}
 
     //send the table to the template using ctx
     //$w->ctx('insightsTable', Html::table($table, '#', '#', $tableHeaders));
--- system/modules/insights/models/InsightService.php
+++ system/modules/insights/models/InsightService.php
@@ -12,62 +12,56 @@ class InsightService extends DbService
     {
         $availableInsights = [];
 
-		// Read insights directory for all insights
-		if ($module === 'all') {
-			foreach($this->w->modules() as $insight) {
-				$availableInsights += $this->getInsightsForModule($insight);
-			}
-		} else {
-			$availableInsights = $this->getInsightsForModule($module);
-		}
-		
-		return $availableInsights;
+        // Read insights directory for all insights
+        if ($module === 'all') {
+            foreach ($this->w->modules() as $insight) {
+                $availableInsights += $this->getInsightsForModule($insight);
+            }
+        } else {
+            $availableInsights = $this->getInsightsForModule($module);
+        }
+
+        return $availableInsights;
     }
 
-    public function getInsightsForModule($insight)
+    public function getInsightsForModule($module)
     {
         $availableInsights = [];
 
         // Check insights folder
-        $module_path = PROJECT_MODULE_DIRECTORY . DS . $insight . DS . MODELS_DIRECTORY;
-        $system_module_path = SYSTEM_MODULE_DIRECTORY . DS . $insight . DS . MODELS_DIRECTORY;
-         $insight_paths = [$module_path, $system_module_path];
-         if (empty($availableInsights[$insight])) {
-             $availableInsights[$insight] = [];
-         }
+        $module_path = PROJECT_MODULE_DIRECTORY . DS . $module . DS . MODELS_DIRECTORY;
+        $system_module_path = SYSTEM_MODULE_DIRECTORY . DS . $module . DS . MODELS_DIRECTORY;
+        $insight_paths = [$module_path, $system_module_path];
+        if (empty($availableInsights[$module])) {
+            $availableInsights[$module] = [];
+        }
 
-          foreach ($insight_paths as $insight_path) {
-              if (is_dir(ROOT_PATH . DS . $insight_path)) {
-                  foreach (scandir(ROOT_PATH . DS . $insight_path) as $file) {
-                      if (!is_dir($file) && $file{
-                          0} !== '.') {
-                          $classname = explode('.',$file);
-                            var_dump($classname);
-                            //check if file is an insight
-                            //if insight add to arry. If not insight skip
-                            //if (insight($classname[1])) {
-                                //if ($this->isInstalled($classname[1])) {
-                                    //$mig = $this->getInsightByClassname($classname[1]);
-                                    //$availableInsights[$insight]
+        foreach ($insight_paths as $insight_path) {
+            if (is_dir(ROOT_PATH . DS . $insight_path)) {
+                foreach (scandir(ROOT_PATH . DS . $insight_path) as $file) {
+                    if (!is_dir($file) && $file{
+                        0} !== '.') {
+                        $classname = explode('.', $file);
+                        //var_dump($classname);
+                        //check if file is an insight
+                        //if insight add to arry. If not insight skip
+                        if (strpos($classname[0], 'Insight') !== false && $classname[0] !== "InsightBaseClass" && $classname[0] !== "InsightService") {
+                            echo "Found insights class; " . $classname[0] . " <br>";
                             //Create instance of class
-                            // $insightspath = $insight_path . DS . $file;
-                            // if (file_exists(ROOT_PATH . DS . $insightspath)) {
-                            //     include_once ROOT_PATH . DS . $insightspath;
-
-                            //     $insight_class = preg_replace('/.php$/', '', $insight_class);
-                            //     if (class_exists($insight_class)) {
-                            //         $insights = (new $insight_class(1))->setWeb($this->w);
-                            //         $availableInsights[$insight][$insight_path . DS . $file] = [
-                            //           $insights->description(),
-                            //           'pretext' => $insights->preText(),
-                            //           'posttext' => $insights->postText()];
-                            //     }
-                            //   }
-                          }
-                      }
-                  }
-              }
-                              return $availableInsights;
+                            $insightspath = $insight_path . DS . $file;
+                            if (file_exists(ROOT_PATH . DS . $insightspath)) {
+                                include_once ROOT_PATH . DS . $insightspath;
+                                if (class_exists($classname[0])) {
+                                    $insight = new $classname[0]($w);
+                                    $availableInsights[$module][] = $insight;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return $availableInsights;
     }
 
     public function getStringContainingInsight($classname)
