0e04fc0a99bbb180346a25e1a8c183e59f225fcc
D.Crannaford <derek@2pisoftware.com>
2020-09-11 12:34:08 +1000

    Admin cest asserting vue model input on forms

--- system/modules/admin/tests/acceptance/LookupCest.php
+++ system/modules/admin/tests/acceptance/LookupCest.php
@@ -7,7 +7,7 @@ class AdminModelsLookupCest
         $I->wantTo('Verify that cmfive admin handles lookups');
         $I->login($I, 'admin', 'admin');
         $I->createUser($I, 'testLookup_testuser', 'password', 'testLookup_test', 'user', 'test@user.com');
-        $I->editUser($I, 'testLookup_testuser', ['title' => 'Prime Minister']);
+        $I->editUser($I, 'testLookup_testuser', ['vModelAutocomplete:title' => 'Prime Minister']);
         $I->clickCmfiveNavbar($I, 'Admin', 'Lookup');
         $I->see('Prime Minister');
         $I->editLookup($I, 'Prime Minister', ["cmfive-modal #title" => 'President']);
@@ -21,7 +21,7 @@ class AdminModelsLookupCest
         $I->see('Cannot delete lookup as it is used as a title for the contacts');
         $I->createLookup($I, 'title', 'The Honerable', 'The Honerable');
         $I->see('The Honerable');
-        $I->editUser($I, 'testLookup_testuser', ['title' => 'The Honerable']);
+        $I->editUser($I, 'testLookup_testuser', ['vModelAutocomplete:title' => 'The Honerable']);
         $I->clickCmfiveNavbar($I, 'Admin', 'Lookup');
         $rowIndex = $I->findTableRowMatching(3, 'President');
         $I->click('Delete', 'tbody tr:nth-child(' . $rowIndex . ')');
