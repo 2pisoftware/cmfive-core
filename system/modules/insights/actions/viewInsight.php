a59f7b1483cf5e043773cacd17cb77d58b8739a4
Alice <alice.hutley@gmail.com>
2020-09-11 14:58:12 +1000

    Editing and working on display individual insights function

--- system/modules/insights/actions/viewInsight.php
+++ system/modules/insights/actions/viewInsight.php
@@ -1,13 +1,24 @@
 <?php
 
 function viewInsight_GET(Web $w) {
+    
+    // now we need to fetch the correct insight
+    // we will use pathMatch to retrieve an insight id from the url.
+    $p = $w->pathMatch('id');
+    // if the id exists we will retrieve the data for that insight otherwise we will return an error. 
+    $insights = !empty($p['id']) ? InsightSerive::getInstance($w)->getInsightForId($p['id']);
+
     //add a title to the action
-    $w->ctx('title','View individual insight');
+    // change the title to reflect viewing insight
+    $w->ctx('title', !empty($p['id']) ? 'View Insight';
 
     // build the table array adding the headers and the row data
     $table = [];
     $tableHeaders = ['Name', 'Module', 'Description', 'Actions'];
-    if (!empty($insights)) {
+    // We now need to change the value for each column to reflect the values of the insight we are viewing only. 
+    if (!empty($p['id'])) {
+        $getUrl = '/insights/viewInsight/' . $insights->id;
+    } 
         foreach ($insights as $insights) {
             $row = [];
             // add values to the row in the same order as the table headers
