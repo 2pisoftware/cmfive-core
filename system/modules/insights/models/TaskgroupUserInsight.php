cba499d6bccd4adef8db5be69b5a03513868ad77
Alice <alice.hutley@gmail.com>
2020-09-18 14:04:48 +1000

    Creating class for displaying insights based on selected user

---
+++ system/modules/insights/models/TaskgroupUserInsight.php
@@ -0,0 +1,26 @@
+<?php
+class TaskgroupUserInsight extends InsightBaseClass
+
+{
+    public $name = "Taskgroup User Insight";
+    public $description = "Displays taskgroups associated with a chosen user";
+
+    //Displays Filters to select user
+    public function getFilters(Web $w): array
+
+    {
+        return [
+            [
+                ["Users", "select", "users", null, AuthService::getInstance($w)->getUsers()]
+            ]
+        ];
+    }
+
+    //Displays insights for selected member
+    public function run(Web $w, $parameters = []): array
+
+    {
+        return TaskService::getInstance($w)->getTaskGroupsForMember($parameters['users']);
+    }
+}
+?>
