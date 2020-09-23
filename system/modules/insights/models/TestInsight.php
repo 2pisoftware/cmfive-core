5ab01e2f4ad689d60a02aceab5c52eed9997dbe2
Alice <alice.hutley@gmail.com>
2020-09-23 10:38:17 +1000

    updating test class to align with InsightBaseClass specifications

--- system/modules/insights/models/TestInsight.php
+++ system/modules/insights/models/TestInsight.php
@@ -6,11 +6,11 @@ class TestInsight extends InsightBaseClass
     public $name = "Test Insight";
     public $description = "Test model for insights";
 
-    public function getFilters()
+    public function getFilters(Web $w): array
     {
     }
 
-    public function run()
+    public function run(Web $w, array $params = []): array
     {
     }
 }
