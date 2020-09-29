7e6ce931105e80945cf999e46a7a7eab7a1e7db4
Jade Short <jadeshort4@gmail.com>
2020-09-29 10:51:07 +1000

    Overide login with email in hook

--- system/modules/auth/auth.hooks.php
+++ system/modules/auth/auth.hooks.php
@@ -1,5 +1,11 @@
 <?php
 
 function auth_admin_remove_user(Web $w, User $user) {
-	return $w->partial("removeUser", ["user" => $user, "redirect" => "/admin-user/remove/" . $user->id], "auth");
+    return $w->partial("removeUser", ["user" => $user, "redirect" => "/admin-user/remove/" . $user->id], "auth");
+}
+
+function auth_admin_update_contact_email(Web $w, User $user) {
+    $user->login = $user->getContact()->email;
+    $user->insertOrUpdate();
+    return;
 }
