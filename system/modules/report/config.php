d8a8acf9fac1194601b6fb6c566b0d5b1f722936
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2020-10-14 11:05:23 +1100

    fix: Config Error Banner
    Added a hook that will set the error variable in context. This will be picked up by the template and display the error as an alert message on all action for admins.
    
    refs: 8396

--- system/modules/report/config.php
+++ system/modules/report/config.php
@@ -3,14 +3,15 @@
 Config::set('report', [
     'active' => true,
     'path' => 'system/modules',
-    "dependencies" => array(
+    "dependencies" => [
         "tecnickcom/tcpdf" => "~6.2",
         "parsecsv/php-parsecsv" => "1.2.0",
-    ),
+    ],
     '__password' => 'maybeconsiderchangingthis',
     'topmenu' => true,
     'hooks' => [
         'admin',
+        'core_web'
     ],
     'database' => [
         'hostname'  => '',
--- system/modules/report/report.hooks.php
+++ system/modules/report/report.hooks.php
@@ -1,16 +1,43 @@
 <?php
 
-function report_core_web_before_get(Web $w) {
-	// build Navigation to Reports for current Module
-	if ($w->Auth->loggedIn()) {
-		$reports = $w->Report->getReportsforNav();
-		if ($reports) {
-			$w->ctx("reports",$reports);
-		}
-	}
+function report_core_web_before_get(Web $w)
+{
+    // build Navigation to Reports for current Module
+    if (AuthService::getInstance($w)->loggedIn()) {
+        $reports = $w->Report->getReportsforNav();
+        if ($reports) {
+            $w->ctx("reports", $reports);
+        }
+    }
 }
 
 // Admin user remove hook
-function report_admin_remove_user(Web $w, User $user) {
-	return $w->partial("removeUser", ["user" => $user, "redirect" => "/admin-user/remove/" . $user->id], "report");
+function report_admin_remove_user(Web $w, User $user)
+{
+    return $w->partial("removeUser", ["user" => $user, "redirect" => "/admin-user/remove/" . $user->id], "report");
+}
+
+/**
+ * This method will be called on every action. If the User is an administrator and the
+ * report database config is not setup correctly, the error context will be set to
+ * display an error message notifying that there is an issue with the config.
+ *
+ * @param Web $w
+ * @return void
+ */
+function report_core_web_after(Web $w)
+{
+    $user = AuthService::getInstance($w)->user();
+    if (empty($user) || !$user->is_admin) {
+        return;
+    }
+
+    $config = Config::get("report.database");
+    if (empty($config)) {
+        return;
+    }
+
+    if (empty($config) || empty($config['database']) || empty($config['username']) || empty($config['password'])) {
+        $w->ctx("error", "Report database connection not setup in config");
+    }
 }
