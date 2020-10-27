d238f022ae7b3a80959efcabd769cadcdbff9933
Alice <alice.hutley@gmail.com>
2020-10-27 12:32:02 +1100

    feat!: displaying member name in table instead of id
    editing the first row of the table to call the correct auth functions for displaying the users full name using the id value
    
    refs: 8865

--- system/modules/insights/actions/manageMembers.php
+++ system/modules/insights/actions/manageMembers.php
@@ -18,9 +18,11 @@ function manageMembers_ALL(Web $w) {
     $tableHeaders = ['Memeber','Is Email Recipient','Role','Actions'];
     if (!empty($memberList)) {
         foreach ($memberList as $member) {
+            //$membername = $member->user_id->getContact();
             $row = [];
             // add values to the row in the same order as the table headers
-            $row[] = $member->user_id;
+            //$row[] = echo memberName;
+            $row[] = AuthService::getInstance($w)->getUser($member->user_id)->getContact()->getFullName();
             $row[] = $member->recieves_emails;
             $row[] = $member->type;
             // the actions column is used to hold buttons that link to actions per item. Note the item id is added to the href on these buttons.
