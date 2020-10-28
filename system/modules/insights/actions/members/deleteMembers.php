bf89fdb8bb87a95489009435f54f6f7fe372880f
Alice <alice.hutley@gmail.com>
2020-10-28 11:12:13 +1100

    feat!: fix delete error urls
    fixing urls for the delete action
    
    refs: 8865

--- system/modules/insights/actions/members/deleteMembers.php
+++ system/modules/insights/actions/members/deleteMembers.php
@@ -10,15 +10,15 @@ function delete_ALL(Web $w) {
     // check to see if the id has been found
     if (empty($p['id'])) {
         // if no id found use the 'error' function to redirect the user to a safe page and display a message.
-        $w->error('No id found for member','insights/manageMembers');
+        $w->error('No id found for member','/insights/manageMembers');
     }
     // use the id to retrieve the member
     $member = InsightService::getInstance($w)->GetMemberForId($p['id']);
     // check to see if the member was found
     if (empty($member)) {
         // no member found so let the user know
-        $w->error('No member found for id','insights/manageMembers');
-        LogService::getInstance($w)->setLogger("INSIGHTS")->error("No member found for that id", "insights/manageMembers");
+        $w->error('No member found for id','/insights/manageMembers');
+        LogService::getInstance($w)->setLogger("INSIGHTS")->error("No member found for that id", "/insights/manageMembers");
     }
     // delete the member
     $member->delete();
