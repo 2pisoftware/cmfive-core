7e5bbc22adb3592b72018ed515a4ec6bb671ff93
Alice <alice.hutley@gmail.com>
2020-11-18 12:26:04 +1100

    feat!: Adding insight class name to runaction
    Getting the runInsight action to display the insight class name for each table
    
    refs: 9425

--- system/modules/insights/actions/runInsight.php
+++ system/modules/insights/actions/runInsight.php
@@ -9,8 +9,9 @@ function runInsight_GET(Web $w)
   $insight = InsightService::getInstance($w)->getInsightInstance($p['insight_class']);
   $run_data = $insight->run($w, $_GET);
   /** @var InsightReportInterface $data */
+  //retrieve correct insight to delete member from and redirect to
   foreach ($run_data as $data) {
-    $w->out('<h3>' . $data->title . "</h3>");
+    $w->out('<h3>' . $data->title . " for " . $p['insight_class'] . '</h3>');
     $w->out(Html::table($data->data, null, "tablesorter", $data->header));
   }
 }
