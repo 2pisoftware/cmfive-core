189481397261beddcebd5002c314c69a5f1013a7
Alice <alice.hutley@gmail.com>
2020-12-22 19:15:01 +1100

    feat!: add error checking
    error checking and send message to logs if error occurs when calling runInsight action.
    
    refs: 9554

--- system/modules/insights/actions/runInsight.php
+++ system/modules/insights/actions/runInsight.php
@@ -9,9 +9,17 @@ function runInsight_GET(Web $w)
   $insight = InsightService::getInstance($w)->getInsightInstance($p['insight_class']);
   $run_data = $insight->run($w, $_GET);
   /** @var InsightReportInterface $data */
+  //Check for errors
+  try {
   //retrieve correct insight to delete member from and redirect to
-  foreach ($run_data as $data) {
-    $w->out('<h3>' . $data->title . " for " . $p['insight_class'] . '</h3>');
-    $w->out(Html::table($data->data, null, "tablesorter", $data->header));
+    foreach ($run_data as $data) {
+      $w->out('<h3>' . $data->title . " for " . $p['insight_class'] . '</h3>');
+      $w->out(Html::table($data->data, null, "tablesorter", $data->header));
+    }
+  } 
+  //catch any fatal errors
+  catch (Error $e) {
+    echo "Error caught: " . $e->getMessage();
+    LogService::getInstance($w)->setLogger("INSIGHTS")->error("Error occurred. Cannot run insight" . $e->getMessage());
   }
 }
