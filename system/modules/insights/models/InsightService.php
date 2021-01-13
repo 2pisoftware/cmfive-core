ab33073e8e66bc4082737b3b32966edfe04a032b
Alice <alice.hutley@gmail.com>
2021-01-13 13:02:06 +1100

    feat: function for formatting date
    function to correctly format date inputs for the database
    
    refs: 9554

--- system/modules/insights/models/InsightService.php
+++ system/modules/insights/models/InsightService.php
@@ -145,4 +145,13 @@ class InsightService extends DbService
     // public function getUserSelectOptions() {
     // return ($this->_db->sql("select u.id as value, concat(c.firstname,' ',c.lastname) as title from user u, contact c where u.contact_id = c.id order by title")->fetchAll());
     // }
+
+    // convert dd/mm/yyyy date to yyy-mm-dd for SQL statements
+    public function date2db($date)
+    {
+        if ($date) {
+            list($d, $m, $y) = preg_split("/\/|-|\./", $date);
+            return $y . "-" . $m . "-" . $d;
+        }
+    }
 }
