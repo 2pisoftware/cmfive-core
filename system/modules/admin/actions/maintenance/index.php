9e941d59b523e48ed37fd87614b7c439b9cd2ce2
Adam Buckley <adam.buckley90@gmail.com>
2021-03-04 12:55:44 +1100

    feat: maintenance admin page (#10)
    - Added a maintenance admin page.
    - Updated search to stop showing deleted objects.

---
+++ system/modules/admin/actions/maintenance/index.php
@@ -0,0 +1,27 @@
+<?php
+
+function index_GET(Web $w)
+{
+    $w->ctx('title', 'Cmfive Maintenance');
+
+    // Get server name
+    $w->ctx('server', php_uname('s m'));
+    $load = null;
+
+    // Get load avg if on linux
+    if (stristr(PHP_OS, "Linux")) {
+        $load = sys_getloadavg();
+    }
+    $w->ctx('load', $load);
+
+    // Get total number of indexed objects
+    $w->ctx('count_indexed', $w->db->get('object_index')->count());
+
+    $w->ctx('audit_row_count', $w->db->get('audit')->count());
+
+    if (Config::get('file.adapters.local.active') !== true) {
+        $w->ctx('cache_image_count', FileService::getInstance($w)->countFilesInDirectory(WEBROOT . '/cache'));
+    }
+
+    $w->ctx("number_of_printers", 0);
+}
