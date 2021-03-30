ae9e67934f01c78c07e98164055753faa2527c6d
Alice <alice.hutley@gmail.com>
2021-03-30 13:36:43 +1100

    feat: allow CSV export
    Editing template and service functions to prepare for exporting to CSV
    [body footer?]
    
    refs: [crm task numbers]
    issues: [github issue numbers]

--- system/modules/insights/models/InsightService.php
+++ system/modules/insights/models/InsightService.php
@@ -153,57 +153,73 @@ class InsightService extends DbService
     }
 
     // export a recordset as CSV
-    public function exportcsv($rows, $title)
+    public function exportcsv($run_data, $title)
     {
+        // echo '<pre>';
+        // var_dump($run_data); die;
         // set filename
         $filename = str_replace(" ", "_", $title) . "_" . date("Y.m.d-H.i") . ".csv";
-
-        // if we have records, comma delimit the fields/columns and carriage return delimit the rows
-        if (!empty($rows)) {
-            foreach ($rows as $row) {
-                // var_dump($row); die;
-                // throw away the first line which list the form parameters
-                //$crumbs = array_shift($row);
-                $title = array_shift($row);
-                $hds = array_shift($row);
-                //$hvals = array_values($hds);
-                $hvals = array_shift($row);
-                
-                // find key of any links
-                // var_dump($hvals);
-                // die;
-                foreach ($hvals as $h) {
-                    if (stripos($h, "_link")) {
-                        list($fld, $lnk) = preg_split("/_/", $h);
-                        $ukey[] = array_search($h, $hvals);
-                        unset($hds[$h]);
-                    }
-                }
-
-                // iterate row to build URL. if required
-                if (!empty($ukey)) {
-                    foreach ($row as $r) {
-                        foreach ($ukey as $n => $u) {
-                            // dump the URL related fields for display
-                            unset($r[$u]);
-                        }
-                        $arr[] = $r;
-                    }
-                    $row = $arr;
-                    unset($arr);
-                }
+        foreach ($run_data as $table) {
+            if (!empty($table)) {
+                $title = $table->title;
+                $hds = $table->header;
                 $csv = new ParseCsv\Csv();
                 $csv->output_filename = $filename;
                 // ignore lib wrapper csv->output, to keep control over header re-sends!
                 
-                $this->w->out($csv->unparse($hvals, $hds, null, null, null));
+                $this->w->out($csv->unparse($table->data, $hds, null, null, null));
                 // can't use this way without commenting out header section, which composer won't like
-                // $this->w->out($csv->output($filename, $row, $hds));
-                unset($ukey);
-            } 
+
+            }
+        }
+
             $this->w->sendHeader("Content-type", "application/csv");
             $this->w->sendHeader("Content-Disposition", "attachment; filename=" . $filename);
             $this->w->setLayout(null); 
-        }
+        // // if we have records, comma delimit the fields/columns and carriage return delimit the rows
+        // if (!empty($rows)) {
+        //     foreach ($rows as $row) {
+        //         // var_dump($row); die;
+        //         // throw away the first line which list the form parameters
+        //         //$crumbs = array_shift($row);
+        //         $title = array_shift($row);
+        //         $hds = array_shift($row);
+        //         //$hvals = array_values($hds);
+        //         $hvals = array_shift($row);
+                
+        //         // find key of any links
+        //         // var_dump($hvals);
+        //         // die;
+        //         foreach ($hvals as $h) {
+        //             if (stripos($h, "_link")) {
+        //                 list($fld, $lnk) = preg_split("/_/", $h);
+        //                 $ukey[] = array_search($h, $hvals);
+        //                 unset($hds[$h]);
+        //             }
+        //         }
+
+        //         // iterate row to build URL. if required
+        //         if (!empty($ukey)) {
+        //             foreach ($row as $r) {
+        //                 foreach ($ukey as $n => $u) {
+        //                     // dump the URL related fields for display
+        //                     unset($r[$u]);
+        //                 }
+        //                 $arr[] = $r;
+        //             }
+        //             $row = $arr;
+        //             unset($arr);
+        //         }
+        //         $csv = new ParseCsv\Csv();
+        //         $csv->output_filename = $filename;
+        //         // ignore lib wrapper csv->output, to keep control over header re-sends!
+                
+        //         $this->w->out($csv->unparse($hvals, $hds, null, null, null));
+        //         // can't use this way without commenting out header section, which composer won't like
+        //         // $this->w->out($csv->output($filename, $row, $hds));
+        //         unset($ukey);
+        //     } 
+            
+        
     }
 }
