df5b7294bcfa6025da585c2cb56643a7e73b40f9
Alice <alice.hutley@gmail.com>
2021-04-28 10:48:22 +1000

    feat: Export to PDF action
    Fetching insight in action for exporting to PDF and planning steps for action in comments
    
    refs: 10244

--- system/modules/insights/actions/export/csv.php
+++ system/modules/insights/actions/export/csv.php
@@ -2,17 +2,17 @@
 function csv_ALL(Web $w)
 {
     $w->setLayout(null);
+    //Find class name of insight
     $p = $w->pathMatch('insight_class');
     if (empty($p['insight_class'])) {
         $w->error('No insight class name found','/insights');
     }
+    //find insight that matches class name
     $insight = InsightService::getInstance($w)->getInsightInstance($p['insight_class']);
     if (empty($insight)) {
         $w->error('No insight found for class name','/insights');
     }
     $run_data = $insight->run($w, $_REQUEST);
 
-// var_dump($rows);
-// die;
     InsightService::getInstance($w)->exportcsv($run_data, $p['insight_class']);
 }
--- system/modules/insights/actions/export/pdf.php
+++ system/modules/insights/actions/export/pdf.php
@@ -1,1 +1,15 @@
 <?php
+function pdf_ALL(Web $w)
+{
+    //Drop-down for chossing template to use for export
+    
+    //Find class name of insight
+    $p = $w->pathMatch('insight_class');
+    //Find insight that matches class name
+    $insight = InsightService::getInstance($w)->getInsightInstance($p['insight_class']);
+
+    //put data for insight in template
+
+    $run_data = $insight->run($w, $_REQUEST);
+    //create service funtion for export to PDF to use here
+}
