2ebbba31440dc5566dc4ccedf53d63af11311c64
Alice <alice.hutley@gmail.com>
2021-04-30 12:16:55 +1000

    feat: Filter template list
    Filter list of template titles to only display those that include the insights name.
    
    refs: 10244

--- system/modules/insights/actions/export/pdf.php
+++ system/modules/insights/actions/export/pdf.php
@@ -1,16 +1,29 @@
 <?php
 function pdf_ALL(Web $w)
 {
-    //Drop-down for chossing template to use for export
-    $chooseTemplate = TemplateService::getInstance($w)->findTemplates(null,null,false,false);
-    
     //Find class name of insight
     $p = $w->pathMatch('insight_class');
     //Find insight that matches class name
     $insight = InsightService::getInstance($w)->getInsightInstance($p['insight_class']);
+    $insight_class_name = $insight->name;
+        
+    //Drop-down for chossing template to use for export
+    $templates = TemplateService::getInstance($w)->findTemplates(null,null,false,false);
+    $choose_template = [];
+        foreach ($templates as $temp_name) {
+            $row = [];
+                if(strpos($temp_name->title, $insight_class_name) !== false){
+                    $row = $temp_name->title;
+                }; 
+                $choose_template = $row;
+        };
+    $template_list = array(
+        array("", "hidden", "insight_class_name", $insight_class_name)
+    );
+    $template_list[] =  array("Template", "select", "title", null, $choose_template);
 
     //put data for insight in template
-
     $run_data = $insight->run($w, $_REQUEST);
+    
     //create service funtion for export to PDF to use here
 }
