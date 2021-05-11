b26aa9e60e2e4105d2832561921462fba2c16839
Alice <alice.hutley@gmail.com>
2021-05-11 12:36:44 +1000

    feat: find corrct template
    Changes to find correct template to display in list, and comments for more changes needed
    
    refs: 10244

--- system/modules/insights/actions/export/pdf.php
+++ system/modules/insights/actions/export/pdf.php
@@ -1,23 +1,23 @@
 <?php
-function pdf_ALL(Web $w)
+function pdf_ALL(Web $w)//GET
 {
     //Find class name of insight
     $p = $w->pathMatch('insight_class');
     //Find insight that matches class name
     $insight = InsightService::getInstance($w)->getInsightInstance($p['insight_class']);
-    $insight_class_name = $insight->name;
-    $insight_class_name_pdf = $insight_class_name . ' PDF';
+    $insight_class_name_pdf = $p['insight_class'] . '_pdf';
         
     //Drop-down for chossing template to use for export
-    $templates = TemplateService::getInstance($w)->findTemplates(null,$insight_class_name_pdf,false,false);
-    var_dump($templates); die;
+    $templates = TemplateService::getInstance($w)->findTemplates('insights',$insight_class_name_pdf,false,false);
+    //var_dump($templates); die;
     $template_list = array(
-        array("", "hidden", "insight_class_name", $insight_class_name)
+        array("", "hidden", "insight_class", $p['insight_class'])
     );
-    $template_list[] =  array("Template", "select", "title", null, $templates);
+    $template_list[] =  array("Template", "select", "template_id", null, $templates);
 
-    //put data for insight in template
-    $run_data = $insight->run($w, $_REQUEST);
-    
-    //create service funtion for export to PDF to use here
 }
+//POST
+//put data for insight in template
+//$run_data = $insight->run($w, $_REQUEST);
+    
+//create service funtion for export to PDF to use here
