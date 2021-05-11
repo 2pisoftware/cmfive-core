25b0e3a7b67e00d712cf866eb61f92dea3e24576
Alice <alice.hutley@gmail.com>
2021-05-11 14:41:18 +1000

    feat: multi-col form drop-down
    Changing drop-down into multi-col form and adding code to be edited for use in yet to be created POSt function
    
    refs: 10244

--- system/modules/insights/actions/export/pdf.php
+++ system/modules/insights/actions/export/pdf.php
@@ -5,15 +5,22 @@ function pdf_ALL(Web $w)//GET
     $p = $w->pathMatch('insight_class');
     //Find insight that matches class name
     $insight = InsightService::getInstance($w)->getInsightInstance($p['insight_class']);
+    $insight_name = $insight->name;
     $insight_class_name_pdf = $p['insight_class'] . '_pdf';
         
     //Drop-down for chossing template to use for export
     $templates = TemplateService::getInstance($w)->findTemplates('insights',$insight_class_name_pdf,false,false);
-    //var_dump($templates); die;
+    //var_dump($insight_name); die;
+    //build form for drop-down
     $template_list = array(
         array("", "hidden", "insight_class", $p['insight_class'])
     );
     $template_list[] =  array("Template", "select", "template_id", null, $templates);
+
+    //Send template to the post method (to be edited)
+    $postUrl = '/insights-members/editMembers/' . (!empty($member->id) ? $member->id : '');
+
+    $w->out(Html::multiColForm([(empty($p['insight_class']) ? "Use template") . " for $insight_name" => [$template_list]], $postUrl));
 
 }
 //POST
