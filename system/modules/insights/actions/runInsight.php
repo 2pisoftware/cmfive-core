3471199e60969f09fcfaf62fe7b7581e4c95f12e
Carsten Eckelmann <carsten@2pisoftware.com>
2021-05-12 10:25:51 +1000

    feat: my timelog insight (#131)
    * [feat]: starting new insight and some fixes to insight base classes
    
    This implements the MyTaskTimeInsight.
    
    refs: 10136
    
    * [feat]: MyTaskTimelog Insight
    
    fixing a few issues with empty data sets
    
    * added more comments to the code
    
    * fix: removed error suppression operator as requested by PR reviewer
    
    * fix: formatted files to standard
    
    Co-authored-by: Taliesin Millhouse <taliesinwrmillhouse@gmail.com>

--- system/modules/insights/actions/runInsight.php
+++ system/modules/insights/actions/runInsight.php
@@ -1,25 +1,26 @@
 <?php
+
 /**@author Alice Hutley <alice@2pisoftware.com> */
 
 function runInsight_GET(Web $w)
 {
 
 
-  /** @var InsightBaseClass $insight */
-  /** this class will be used on all other classes accessed by the runInsight action. It sets up the initial parameters for each insight */
+    /** @var InsightBaseClass $insight */
+    /** this class will be used on all other classes accessed by the runInsight action. It sets up the initial parameters for each insight */
 
-  $p = $w->pathMatch('insight_class');
-  $w->ctx('insight_class_name', $p['insight_class']);
-  $insight = InsightService::getInstance($w)->getInsightInstance($p['insight_class']);
-  $w->ctx('insight', $insight);
-  $w->ctx('title', $insight->name);
-  //var_dump($_GET);
-  //die;
-  $run_data = $insight->run($w, $_GET);
-  /** @var InsightReportInterface $data */
-  /**Defines how the insight table should look. Gives column heading for the data defined in each insight class */
-  $w->ctx('run_data', $run_data);
-  
-  //build request string for editing parameters
-  $w->ctx('request_string', http_build_query($_GET));
+    $p = $w->pathMatch('insight_class');
+    $w->ctx('insight_class_name', $p['insight_class']);
+    $insight = InsightService::getInstance($w)->getInsightInstance($p['insight_class']);
+    $w->ctx('insight', $insight);
+    $w->ctx('title', $insight->name);
+    //var_dump($_GET);
+    //die;
+    $run_data = $insight->run($w, $_GET);
+    /** @var InsightReportInterface $data */
+    /**Defines how the insight table should look. Gives column heading for the data defined in each insight class */
+    $w->ctx('run_data', $run_data);
+
+    //build request string for editing parameters
+    $w->ctx('request_string', http_build_query($_GET));
 }
