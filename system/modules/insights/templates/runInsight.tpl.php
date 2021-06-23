819d57054f52711f2c0a3858373852f148703621
Alice <alice.hutley@gmail.com>
2021-06-23 14:37:36 +1000

    feat: re-create form
    Recreating the form in the same style as the cmfive standard in the template. Buttons are now editable for closing. Parameters for select option yet to be moved.
    
    refs: 10244

--- system/modules/insights/templates/runInsight.tpl.php
+++ system/modules/insights/templates/runInsight.tpl.php
@@ -1,7 +1,8 @@
 <?php
 echo Html::b("/insights/viewInsight/" . $insight_class_name . "?" . $request_string, "Change Insight Parameters");
 echo Html::b("/insights-export/csv/" . $insight_class_name . "?" . $request_string, "Export to CSV", null);
-echo Html::b("#", "Export to PDF", null, $pdf, false, null, null, null);
+//echo Html::b("#", "Export to PDF", null, $pdf, false, null, null, null);
+echo Html::box("/insights-export/pdf/" . $insight_class_name . "?" . $request_string, "Export to PDF", false, false, null, null, "isbox", null, null, null, 'cmfive-modal');
 
 //Check for errors
 try {
@@ -10,15 +11,35 @@ try {
         echo '<h4>' . $data->title . '</h4>';
         echo Html::table($data->data, null, "tablesorter", $data->header);
       }
-    } 
+    }
     //catch any fatal errors
     catch (Error $e) {
       echo "Error caught: " . $e->getMessage();
       LogService::getInstance($w)->setLogger("INSIGHTS")->error("Error occurred. Cannot run insight $p" . $e->getMessage());
     }
-//div GET item on to become visible id($pdf)
-<div style:visible=hidden aligned:position=absolute>
-//Select field goes in here along with save and cancel options
+?>
+<!--div GET item on to become visible id($pdf)-->
+<div id="cmfive-modal" class="reveal-modal xlarge open" data-reveal=""  style="display:block; opacity:1; visibility:hidden; top:100px" aligned:position=absolute>
+  <form class=" small-12 columns">
+    <div class="row-fluid clearfix small-12 multicolform">
+      <div class="panel clearfix">
+        <div class="row-fluid clearfix section-header"></div>
+        <ul class="small-block-grid-1 medium-block-grid-2 section-body">
+          <li>
+            <label class="small-12 columns">
+              <select style="visibility:visible"></select>
+            </label>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row small-12 columns">
+      <button class="button tiny tiny button savebutton"></button>
+      <button class="button tiny tiny button cancelbutton"></button>
+    </div>
+  </form>
 </div>
-//script goes at bottom
-//js get elemt by id and send data to post function in pdf action
+<!--Select field goes in here along with save and cancel options. Rest of get goes in reunInsight action-->
+</div>
+<!--script goes at bottom
+js get elemt by id and send data to post function in pdf action-->
