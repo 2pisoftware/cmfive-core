7dbb63bd06973f52612b16511c52de86eaa8691b
Alice <alice.hutley@gmail.com>
2021-07-06 15:33:46 +1000

    feat: variables and close
    Creating variables for modal, button, and modal elements in javascript and adding function to display modal and make modal disappear
    
    refs: 10244

--- system/modules/insights/templates/runInsight.tpl.php
+++ system/modules/insights/templates/runInsight.tpl.php
@@ -20,7 +20,7 @@ catch (Error $e) {
 ?>
 
 <!--div GET item on to become visible id($pdf)-->
-<div id="insight_pdf_modal" class="reveal-modal xlarge open" data-reveal=""  style="display:block; opacity:1; visibility:hidden; top:100px" aligned:position=absolute>
+<div id="insight_pdf_modal" class="reveal-modal xlarge open" data-reveal=""  style="display:none; opacity:1; visibility:hidden; top:100px" aligned:position=absolute>
   <form class=" small-12 columns">
     <div class="row-fluid clearfix small-12 multicolform">
       <div class="panel clearfix">
@@ -35,8 +35,8 @@ catch (Error $e) {
       </div>
     </div>
     <div class="row small-12 columns">
-      <button class="button tiny tiny button savebutton"></button>
-      <button class="button tiny tiny button cancelbutton"></button>
+      <button id="save_button" class="button tiny tiny button savebutton"></button>
+      <button id="cancel_button" class="button tiny tiny button cancelbutton"></button>
     </div>
   </form>
 <!--Select field goes in here along with save and cancel options. Rest of get goes in reunInsight action-->
@@ -44,11 +44,28 @@ catch (Error $e) {
 <!--script goes at bottom
 js get element by id and send data to post function in pdf action-->
 <script>
-  //variable for modal and button needed
+  //variable for modal and button
+  let my_pdf_button = document.getElementById('my_pdf_button')
+  let insight_pdf_modal = document.getElementById('insight_pdf_modal')
   //override onclick function of button
   //onclick sets modal visibiltiy to visible
+  my_pdf_button.onclick = function(){
+    insight_pdf_modal.style.display = "block"
+    insight_pdf_modal.style.visibility = "visible"
+  }
   //variable needed for save and cancel buttons
+  let save_button = document.getElementById("save_button")
+  let cancel_button = document.getElementById("cancel_button")
   //override onclick of cancel button. Sets visibiltiy back to hidden
+  cancel_button.onclick = function(){
+    insight_pdf_modal.style.display = "none"
+  }
   //override onclick of save button. Send data to post funtion of PDF action (use ajax). Then close modal.
+  //close when click outside of modal
+  window.onclick = function(e){
+    if(e.target == insight_pdf_modal){
+      insight_pdf_modal.style.display = "none"
+    }
+  }
   console.log('hello my_modal');
 </script>
