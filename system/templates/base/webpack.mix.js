ebda3a4628755be6b7f03714525bfe1649f0838b
Adam Buckley <adam@2pisoftware.com>
2021-07-07 05:24:01 +0000

    Can compile assets in other project modules

--- system/templates/base/webpack.mix.js
+++ system/templates/base/webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require("laravel-mix");
 const fs = require("fs");
 // const path = require("path");
 
-// async function loadAssets() {
+async function loadAssets() {
     mix
         .ts("src/js/app.ts", "")
         .vue()
@@ -22,27 +22,27 @@ const fs = require("fs");
                 while ((ts_asset_dir_ent = ts_asset_dir.readSync()) !== null) {
                     if (ts_asset_dir_ent.name.split('.').pop() == 'ts') {
                         console.log('Compiling', ts_asset_dir_ent.name)
-                        mix.ts(BASE_PATH + module_dir_ent.name + '/assets/ts/' + ts_asset_dir_ent.name, '').vue()
+                        mix.ts(BASE_PATH + module_dir_ent.name + '/assets/ts/' + ts_asset_dir_ent.name, 'dist/' + module_dir_ent.name).vue()
                             .setPublicPath('dist')
-                            .setResourceRoot('/system/templates/base/dist/' + module_dir_ent.name + '/');
+                            .setResourceRoot('/system/templates/base/dist/');
                     }
                 }
                 ts_asset_dir.closeSync();
             }
 
             if (fs.existsSync(BASE_PATH + module_dir_ent.name + '/assets/scss') && fs.lstatSync(BASE_PATH + module_dir_ent.name + '/assets/scss').isDirectory()) {
-                const scss_asset_dir = fs.opendirSync(BASE_PATH + module_dir_ent.name + '/assets/ts');
+                const scss_asset_dir = fs.opendirSync(BASE_PATH + module_dir_ent.name + '/assets/scss');
                 let scss_asset_dir_ent;
                 while ((scss_asset_dir_ent = scss_asset_dir.readSync()) !== null) {
+                    console.log('Found sass file', scss_asset_dir_ent.name)
                     if (scss_asset_dir_ent.name.split('.').pop() == 'scss') {
-                        console.log('Compiling', scss_asset_dir_ent.name)
-                        mix.sass(BASE_PATH + module_dir_ent.name + '/assets/scss/' + scss_asset_dir_ent.name, '', {
+                        mix.sass(BASE_PATH + module_dir_ent.name + '/assets/scss/' + scss_asset_dir_ent.name, 'dist/' + module_dir_ent.name, {
                             sassOptions: {
-                                includePaths: ['/system/templates/base/src/scss/']
+                                includePaths: [__dirname + '/src/scss/']
                             }
                         })
-                        .setPublicPath('dist')
-                        .setResourceRoot('/system/templates/base/dist/' + module_dir_ent.name + '/');
+                        .setPublicPath('dist/')
+                        .setResourceRoot('/system/templates/base/dist/');
                     }
                 }
                 scss_asset_dir.closeSync()
@@ -50,15 +50,6 @@ const fs = require("fs");
         }
     }
     parent_dir.closeSync();
-// }
+}
 
-// loadAssets();
-// mix
-//     .ts('../../../../../../../modules/**/assets/ts/*', 'dist/').vue()
-//     .sass('../../../../../../../modules/bridge/assets/scss/app.scss', '', {
-//         sassOptions: {
-//             includePaths: ['/system/templates/base/src/scss']
-//         }
-//     })
-//     .setPublicPath('dist')
-//     .setResourceRoot('/system/templates/base/dist');
+loadAssets();
