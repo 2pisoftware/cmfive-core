853a33ed450ff92c340bcb2d77927d01c5dce606
Adam Buckley <adam@2pisoftware.com>
2021-11-25 00:31:47 +0000

    fix: added login label support to forgot password

--- system/modules/auth/templates/forgotpassword.tpl.php
+++ system/modules/auth/templates/forgotpassword.tpl.php
@@ -1,6 +1,6 @@
 <form method="POST" action="/auth/forgotpassword">
     <input type="hidden" name="<?php echo CSRF::getTokenID(); ?>" value="<?php echo CSRF::getTokenValue(); ?>" />
-    <label for="login">Login</label>
+    <label for="login"><?php echo Config::get('auth.login_label', 'Login'); ?></label>
     <input id="login" name="login" type="text" placeholder="Your login" />
     <button type="submit" class="button large-5 small-12">Submit</button>
     <button type="button" onclick='window.location.href="/auth/login"' class="button secondary large-5 small-12 right">Back</button>
