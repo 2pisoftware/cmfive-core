62c169e6481ec1f30dde05ba062981f7ab2d5427
Adam Buckley <adam@2pisoftware.com>
2021-11-30 01:53:38 +0000

    fix: fixes to insights

--- system/modules/insights/actions/members/editMembers.php
+++ system/modules/insights/actions/members/editMembers.php
@@ -1,19 +1,20 @@
 <?php
 
-/**@author Alice Hutley <alice@2pisoftware.com> */
-
-// provide form by which to add members to an insight
+/**
+ * Provide form by which to add members to an insight
+ *
+ * @author Alice Hutley <alice@2pisoftware.com>
+ */
 function editMembers_GET(Web &$w)
 {
-
 	//We now need to check if we are adding a new members or editing an existing member
-	//We will use pathmatch to retrieve a member id from the yrl
+	//We will use pathmatch to retrieve a member id from the url
 	$p = $w->pathMatch('id');
 	//if the id exists we will retrieve the data for that member. Otherwise we will add a new member
 	$member = !empty($p['id']) ? InsightService::getInstance($w)->getMemberForId($p['id']) : new InsightMembers($w);
 
 	//retrieve correct insight to add new member to
-	$insight_class_name = !empty($member->id) ? $member->insight_class_name : $w->request('insight_class');
+	$insight_class_name = !empty($member->id) ? $member->insight_class_name : Request::string('insight_class');
 
 	//action title for adding new memeber and editing existing memeber
 	$insight = InsightService::getInstance($w)->getInsightInstance($insight_class_name);
@@ -21,40 +22,29 @@ function editMembers_GET(Web &$w)
 
 
 	// get the list of users that can be added to the insight
-	$userstoadd = AuthService::getInstance($w)->getUsers();
+	$userstoadd = array_filter(AuthService::getInstance($w)->getUsers(), function ($u) {
+		return $u->hasAnyRole(['insights_user', 'insights_admin']);
+	});
 	$members = $insight->getMembers($w);
 
 	// strip the dumplicates. dealing with an object so no quick solution
-	$users = array();
+	$users = [];
 	foreach ($userstoadd as $user) {
 		if (!in_array($user, $users, true)) {
-			//if (!InsightService::getInstance($w)->IsMember($insight_class_name, $user->id)){
-			//var_dump(array_search($user->id, array_column($members, 'user_id')));
-			//die;
 			if (array_search($user->id, array_column($members, 'user_id')) === FALSE) {
 				$users[] = $user;
 			}
 		}
 	}
 
-	// build form
-	// if (!empty($p['id'])) {
-	// $addMemberForm = array(
-	//     array("","hidden", "insight_class_name", $insight_class_name);
-	// 	array("Add Member","select","user_id",null,$users);
-	//     array("With Role","select","type",$member->type,$w->Insight->getInsightPermissions());
-	// );
-	// else
-	// AuthService::getInstance($w)->getUser($member->user_id)->getContact()->getFullName();
-	$addMemberForm = array(
-		array("", "hidden", "insight_class_name", $insight_class_name)
-	);
+	$addMemberForm = [["", "hidden", "insight_class_name", $insight_class_name]];
+
 	if (empty($p['id'])) {
-		$addMemberForm[] = array("Add Member", "select", "user_id", null, $users);
+		$addMemberForm[] = ["Add Member", "select", "user_id", null, $users];
 	} else {
-		$addMemberForm[] = array("Add member", "text", "-user_id", AuthService::getInstance($w)->getUser($member->user_id)->getContact()->getFullName());
+		$addMemberForm[] = ["Add member", "text", "-user_id", AuthService::getInstance($w)->getUser($member->user_id)->getContact()->getFullName()];
 	}
-	$addMemberForm[] =  array("With Role", "select", "type", $member->type, $w->Insight->getInsightPermissions());
+	$addMemberForm[] =  ["With Role", "select", "type", $member->type, InsightService::getInstance($w)->getInsightPermissions()];
 
 	//if we are editing an existing meber we need to send the id to the post method
 	$postUrl = '/insights-members/editMembers/' . (!empty($member->id) ? $member->id : '');
@@ -68,15 +58,14 @@ function editMembers_POST(Web $w)
 
 	//As in the get function we need to check if we are editing an exisiting member
 	$p = $w->pathMatch('id');
-	$member = !empty($p['id']) ? InsightService::getInstance($w)->GetMemberForId($p['id']) : new InsightMembers($w);
+	$member = !empty($p['id']) ? InsightService::getInstance($w)->getMemberForId($p['id']) : new InsightMembers($w);
 
 	//use the fill function to fill input field data into properties with matching names
 	if (empty($member->id)) {
 		$member->fill($_POST);
 	} else {
-		$member->type = $w->request('type');
+		$member->type = Request::string('type');
 	}
-
 
 	// function for saving to database
 	$member->insertOrUpdate();
