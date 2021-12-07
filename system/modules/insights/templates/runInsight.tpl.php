0748606cf14a0922e2e865910aa16b4a8ef2ccbe
Alice <alice.hutley@gmail.com>
2021-12-07 23:36:10 +1100

    feat: Fixing foundation
    Working to fix issue with foundation function
    
    refs: 10151

--- system/modules/insights/templates/runInsight.tpl.php
+++ system/modules/insights/templates/runInsight.tpl.php
@@ -50,16 +50,24 @@ catch (Error $e) {
 </div>
 
 <script>
-  //variable for modal and button
+  document.addEventListener('DOMContentLoaded', function(){ 
+    // your code goes here
+    //variable for modal and button
   let my_pdf_button = document.getElementById('my_pdf_button');
   let insight_pdf_modal = document.getElementById('insight_pdf_modal');
+
+  console.log(document.getElementById("insight_pdf_modal"));
+  console.log($("#insight_pdf_modal"));
+
+
 
   //override onclick function of button
   my_pdf_button.onclick = function() {
     document.getElementById("insight_pdf_modal").foundation("reveal", "open");
   }
 
-  document.getElementById("pdf-close-modal").click(function(event)=>.getElementById("insight_pdf_modal").foundation("reveal", "close");
+  document.getElementById("pdf-close-modal").click(function(event){
+    document.getElementById("insight_pdf_modal").foundation("reveal", "close");
     if (document.getElementById(this).hasClass("close-reveal-modal")) {
     } else {
       // No one is using the help system at the moment
@@ -68,20 +76,23 @@ catch (Error $e) {
     }
 
     return false;
-  );
+  });
 
   //set variables for save and cancel buttons
   let export_button = document.getElementById("pdf_export_button");
   let cancel_button = document.getElementById("pdf_cancel_button");
 
   //override onclick of cancel button. Sets visibiltiy back to hidden
-  document.getElementById("pdf_cancel_button").click(function(event)=>
+  document.getElementById("pdf_cancel_button").click(function(event){
     event.preventDefault();
     document.getElementById("insight_pdf_modal").foundation("reveal", "close");
-  );
+  });
 
   //override onclick of save button. Info sent to post function in HTML form. Close modal.
   document.getElementById("pdf_form").submit(function() {
     document.getElementById("insight_pdf_modal").foundation("reveal", "close");
   });
+}, false);
+
+  
 </script>
