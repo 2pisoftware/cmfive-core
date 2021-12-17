77584dd330553c0cb7428e2b3d92764dec64dc8c
Adam Buckley <adam@2pisoftware.com>
2021-12-17 00:18:26 +0000

    feat: add test mode banner support to login screen

--- system/modules/auth/templates/layout-foundation-5.tpl.php
+++ system/modules/auth/templates/layout-foundation-5.tpl.php
@@ -27,6 +27,13 @@
     <body>
         <div class="row">
             <div class="large-6 small-10 columns small-centered">
+                <?php if (Config::get('system.test_mode', false) === true) : ?>
+                    <div class="row">
+                        <div data-alert class="alert-box alert-info">
+                            <?php echo Config::get('system.test_mode_message'); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
                 <div class="row small-6 small-centered">
                     <?php if (Config::get("main.application_logo")) : ?>
                         <center><img src="<?php echo Config::get("main.application_logo");?>" alt="<?php echo Config::get("main.application_name");?>"/></center>
