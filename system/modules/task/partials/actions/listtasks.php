f773dc5b0d7374dbd7fb054d5f0c95b7eba67b40
Dane <dane@2pisoftware.com>
2022-02-18 09:26:01 +1100

    fix codesniffer issues

--- system/modules/task/partials/actions/listtasks.php
+++ system/modules/task/partials/actions/listtasks.php
@@ -1,48 +1,42 @@
 <?php namespace System\Modules\Task;
 
-function listtasks(\Web $w, $params = array()) 
+function listtasks(\Web $w, $params = array())
 {
     $w->ctx("redirect", $params['redirect']);
     
-	$w->ctx("hide_filter", array_key_exists('hide_filter', $params) ? $params['hide_filter'] : false);
+    $w->ctx("hide_filter", array_key_exists('hide_filter', $params) ? $params['hide_filter'] : false);
     $w->ctx("hide_columns", array_key_exists('columns_to_hide', $params));
-	$w->ctx("columns_to_hide", array_key_exists('columns_to_hide', $params)? $params['columns_to_hide'] : []);
+    $w->ctx("columns_to_hide", array_key_exists('columns_to_hide', $params)? $params['columns_to_hide'] : []);
 
     $taskgroup = null;
-    if (!empty($params['task_group_id']))
-    {
+    if (!empty($params['task_group_id'])) {
         $task_group_id = $params['task_group_id'];
         $taskgroup = $w->Task->getTaskGroup($task_group_id);
     }
     
-    if (!empty($params['task_status']))
-    {
+    if (!empty($params['task_status'])) {
         $task_status = $params['task_status'];
     }
     
     // Look for reset
     $reset = $w->request("filter_reset_task_list");
-    if (empty($reset))
-    {
+    if (empty($reset)) {
         // Get filter values
         $assignee_id = $w->request("assignee_id");
         $creator_id = $w->request("creator_id");
 
-        if (empty($taskgroup))
-        {
-	        $request_task_group_id = $w->request("task_group_id");
-	        if (!empty($request_task_group_id))
-            {
-	            $task_group_id = $request_task_group_id;
-	        }
+        if (empty($taskgroup)) {
+            $request_task_group_id = $w->request("task_group_id");
+            if (!empty($request_task_group_id)) {
+                $task_group_id = $request_task_group_id;
+            }
         }
         
         $task_type = $w->request('task_type');
         $task_priority = $w->request('task_priority');
         
         $request_task_status = $w->request('task_status');
-        if (!empty($request_task_status))
-        {
+        if (!empty($request_task_status)) {
             $task_status = $request_task_status;
         }
         
@@ -55,50 +49,39 @@ function listtasks(\Web $w, $params = array())
     $query_object = $w->db->get("task")->leftJoin("task_group");
     
     // We can now make ID queries directly to the task_group table because of left join
-    if (!empty($task_group_id))
-    {
+    if (!empty($task_group_id)) {
         $query_object->where("task_group.id", $task_group_id);
     }
     
     // Repeat above for everything else
-    if (!empty($assignee_id))
-    {
+    if (!empty($assignee_id)) {
         $query_object->where("task.assignee_id", $assignee_id);
     }
-    if (!empty($creator_id))
-    {
+    if (!empty($creator_id)) {
         // $query_object->where("task.creator_id", $creator_id);
     }
-    if (!empty($task_type))
-    {
+    if (!empty($task_type)) {
         $query_object->where("task.task_type", $task_type);
     }
-    if (!empty($task_priority))
-    {
+    if (!empty($task_priority)) {
         $query_object->where("task.priority", $task_priority);
     }
-    if (!empty($task_status))
-    {
+    if (!empty($task_status)) {
         $query_object->where("task.status", $task_status);
     }
-    if (!empty($is_closed))
-    {
+    if (!empty($is_closed)) {
         $query_object->where("task.is_closed", ((is_null($is_closed) || $is_closed == 0) ? 0 : 1));
-    } 
-    else 
-    {
+    } else {
         $query_object->where("task.is_closed", 0);
     }
     //do not retrieve inactive tasks
     $query_object->where("task.is_active", 1);
     
     // This part is why we want to make our query manually
-    if (!empty($dt_from))
-    {
+    if (!empty($dt_from)) {
         $query_object->where("task.dt_due >= ?", $dt_from);
     }
-    if (!empty($dt_to))
-    {
+    if (!empty($dt_to)) {
         $query_object->where("task_dt_due <= ?", $dt_to);
     }
     
@@ -112,26 +95,20 @@ function listtasks(\Web $w, $params = array())
     
     // Build the filter and its data
     $taskgroup_data = null;
-    if (empty($taskgroup))
-    {
-    	$taskgroup_data = $w->Task->getTaskGroupDetailsForUser();
-    }
-    else
-    {
-    	$taskgroup_data = $w->Task->getTaskGroupDetailsForTaskGroup($task_group_id);
+    if (empty($taskgroup)) {
+        $taskgroup_data = $w->Task->getTaskGroupDetailsForUser();
+    } else {
+        $taskgroup_data = $w->Task->getTaskGroupDetailsForTaskGroup($task_group_id);
     }
     $filter_data = array();
     $filter_data[] = array("Assignee", "select", "assignee_id", !empty($assignee_id) ? $assignee_id : null, $taskgroup_data["members"]);
     $filter_data[] = array("Creator", "select", "creator_id", !empty($creator_id) ? $creator_id : null, $taskgroup_data["members"]);
     
-    if (empty($taskgroup))
-    {
-    	$filter_data[] = array("Task Group", "select", "task_group_id", !empty($task_group_id) ? $task_group_id : null, $taskgroup_data["taskgroups"]);
-    }
-    else
-    {
-    	$filter_data[] = array("Task Group", "static", "taskgroupname", $taskgroup->title);
-    	$filter_data[] = array("", "hidden", "task_group_id", !empty($task_group_id) ? $task_group_id : null);
+    if (empty($taskgroup)) {
+        $filter_data[] = array("Task Group", "select", "task_group_id", !empty($task_group_id) ? $task_group_id : null, $taskgroup_data["taskgroups"]);
+    } else {
+        $filter_data[] = array("Task Group", "static", "taskgroupname", $taskgroup->title);
+        $filter_data[] = array("", "hidden", "task_group_id", !empty($task_group_id) ? $task_group_id : null);
     }
     $filter_data[] = array("Task Type", "select", "task_type", !empty($task_type) ? $task_type : null, $taskgroup_data["types"]);
     $filter_data[] = array("Task Priority", "select", "task_priority", !empty($task_priority) ? $task_priority : null, $taskgroup_data["priorities"]);
@@ -191,5 +168,4 @@ function listtasks(\Web $w, $params = array())
     //     // display list
     //     $w->ctx("notify", \Html::table($line, null, "tablesorter", true));
     // }
-    
 }
--- system/modules/task/partials/templates/listtasks.tpl.php
+++ system/modules/task/partials/templates/listtasks.tpl.php
@@ -1,31 +1,24 @@
-<?php 
+<?php
 echo (empty($hide_filter) || $hide_filter !== true) ? Html::filter("Filter Tasks", $filter_data, '/' . $redirect, "GET", "Filter", "task_list") : '';
 
-if (!empty($tasks)) 
-{
+if (!empty($tasks)) {
     $table_header = array("Title", "Created By", "Assigned To", "Group", "Type", "Priority", "Status", "Due");
     $table_data = array();
-    if ($hide_columns)
-    { 
-        if (is_array($columns_to_hide))
-        {
+    if ($hide_columns) {
+        if (is_array($columns_to_hide)) {
             $columns_to_hide = array_combine($columns_to_hide, $columns_to_hide);
             $table_header = array_diff_key($table_header, $columns_to_hide);
-        } 
-        else 
-        {
+        } else {
             $hide_columns = false;
         }
     }
 
     // Build table data
     usort($tasks, array("TaskService", "sortTasksbyDue"));
-    foreach ($tasks as $task)
-    {
-        if ($task->getCanIView())
-        {
+    foreach ($tasks as $task) {
+        if ($task->getCanIView()) {
             $table_line = array();
-			$table_line[] = Html::a("/task/edit/" . $task->id, $task->title);
+            $table_line[] = Html::a("/task/edit/" . $task->id, $task->title);
 
             // Append the rest of the data
             $table_line += array(null,
@@ -38,16 +31,13 @@ if (!empty($tasks))
                 $task->isTaskLate()
             );
 
-            if ($hide_columns)
-            {
+            if ($hide_columns) {
                 $table_line = array_diff_key($table_line, $columns_to_hide);
             }
             $table_data[] = $table_line;
         }
     }
     echo Html::table($table_data, null, "tablesorter", $table_header);
-}
-else 
-{ ?>
+} else { ?>
     <h3><small>No tasks found.</small></h3>
 <?php }
