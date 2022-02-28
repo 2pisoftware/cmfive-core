c35a78c8851b0aed6e88abd65c69f62e6ee037a4
Adam Buckley <adam.buckley90@gmail.com>
2022-03-01 09:58:37 +1100

    fix: cleaned up code and removed long array syntax

--- system/modules/admin/actions/templates/edit.php
+++ system/modules/admin/actions/templates/edit.php
@@ -11,52 +11,45 @@ function edit_GET(Web $w)
 	$t = TemplateService::getInstance($w)->getTemplate($p['id']);
 	$t = $t ? $t : new Template($w);
 
-	$newForm = array();
-	$newForm["Template Details"] = array(
-		array(
-			array("Title", "text", "title", $t->title),
-			array("Active", "checkbox", "is_active", $t->is_active)
-		),
-		array(
-			array("Module", "text", "module", $t->module),
-			array("Category", "text", "category", $t->category)
-		)
-	);
-	$newForm['Description'] = array(
-		array(
-			array("", "textarea", "description", $t->description)
-		),
-	);
+	$newForm = [];
+	$newForm["Template Details"] = [
+		[
+			["Title", "text", "title", $t->title],
+			["Active", "checkbox", "is_active", $t->is_active]
+		],
+		[
+			["Module", "text", "module", $t->module],
+			["Category", "text", "category", $t->category]
+		]
+	];
+	$newForm['Description'] = [
+		[
+			["", "textarea", "description", $t->description]
+		],
+	];
 
 	$w->ctx("editdetailsform", Html::multiColForm($newForm, $w->localUrl('/admin-templates/edit/' . $t->id)));
 
-	$newForm = array();
-	$newForm["Template Title"] = array(
-		array(array("", "textarea", "template_title", $t->template_title, 100, 1, false))
-	);
-	$newForm["Template Body"] = array(
-		array(array("", "textarea", "template_body", $t->template_body, 60, 100, "codemirror"))
-	);
+	$newForm = [];
+	$newForm["Template Title"] = [
+		[["", "textarea", "template_title", $t->template_title, 100, 1, false]]
+	];
+	$newForm["Template Body"] = [
+		[["", "textarea", "template_body", $t->template_body, 60, 100, "codemirror"]]
+	];
 
 	$w->ctx("templateform", Html::multiColForm($newForm, $w->localUrl('/admin-templates/edit/' . $t->id)));
 
-	$newForm = array();
-	$newForm["Title Data"] = array(
-		array(array("", "textarea", "test_title_json", $t->test_title_json, 100, 5, false))
-	);
-	$newForm["Body Data"] = array(
-		array(array("", "textarea", "test_body_json", $t->test_body_json, 100, 20, false))
-	);
+	$newForm = [];
+	$newForm["Title Data"] = [
+		[["", "textarea", "test_title_json", $t->test_title_json, 100, 5, false]]
+	];
+	$newForm["Body Data"] = [
+		[["", "textarea", "test_body_json", $t->test_body_json, 100, 20, false]]
+	];
 
 	$w->ctx("testdataform", Html::multiColForm($newForm, $w->localUrl('/admin-templates/edit/' . $t->id)));
 	$w->ctx("id", $p['id']);
-	//        try {
-	//            $w->ctx("testtitle",$t->testTitle());
-	//            $w->ctx("testbody",$t->testBody());
-	//        } catch (Exception $e) {
-	//            $w->ctx("testbody", "Error: Couldn't not render Twig template.<br/>Error Message: " . $e->getMessage());
-	//        }
-
 }
 
 function edit_POST(Web $w)
