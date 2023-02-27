44b81a2e9cd622fbfd4a27ecd4466b67fea2ae8c
D.Crannaford <derek@2pisoftware.com>
2023-02-27 15:58:39 +1100

    Lint&prove usable

--- system/modules/admin/models/AuditInsightObject.php
+++ system/modules/admin/models/AuditInsightObject.php
@@ -8,17 +8,15 @@ class AuditInsightObject extends InsightBaseClass
     //Displays Filters to select user
     public function getFilters(Web $w, $parameters = []): array
     {
-        // $moduleSelectOptions = AuditService::getInstance($w)->getLoggedModules();
-        // $actionSelectOptions = AuditService::getInstance($w)->getLoggedActions();
 
         return [
             "Options" => [
                 [
                     [
-                        "Class", "input", "class",  null
+                        "Object Class", "text", "class",  ""
                     ],
                     [
-                        "Id", "input", "id", null
+                        "Object Id", "text", "id", ""
                     ],
                 ],
             ]
@@ -28,45 +26,41 @@ class AuditInsightObject extends InsightBaseClass
     //Displays insights for selections made in the above "Options"
     public function run(Web $w, $parameters = []): array
     {
-        //below service is referred to as $where in subsequent notes in this block for purpose of examples
-        $data = AuditService::getInstance($w)->getObject(($parameters['class']), ($parameters['id']),false);
-
-$cached =   $obj = !empty(self::$_cache[$parameters['class']][$parameters['id']]) ? self::$_cache[$parameters['class']][$parameters['id']] : null;
-
-$o = new $parameters['class']($w);
-        $table = $o->getDbTableName();
-            AuditService::getInstance($w)->_db->get($table)->where($o->getDbColumnName('id'), $parameters['id']);
-
-//        AuditService::getInstance($w)->buildSelect($o, $table, $parameters['class']);
-    AuditService::getInstance($w)->_db->select(implode(",",$o->getDbTableColumnNames()));
-      
-
-      $result = AuditService::getInstance($w)->_db->fetchRow();
- $o->fill($result, false);
-
+        // do as getObject without caching
+        $data = AuditService::getInstance($w)->getObject(($parameters['class']), ($parameters['id']), false);
 
         if (!$data) {
-             $results[] = new InsightReportInterface('Object Report', ['Results'], [['No data returned for selections']]);
+            $results[] = new InsightReportInterface('Object Report', ['Results'], [['No data returned for selections']]);
         } else {
+            // do as PDO select without any overrides (skip builder)
+            $o = new $parameters['class']($w);
+            $table = $o->getDbTableName();
+            AuditService::getInstance($w)->_db->get($table)->where($o->getDbColumnName('id'), $parameters['id']);
+            AuditService::getInstance($w)->_db->select(implode(",", $o->getDbTableColumnNames()));
+            $result = AuditService::getInstance($w)->_db->fetchRow();
+            // do as would apply to getObject if the select builder didn't exist
+            $o->fill($result, false);
             // convert $data from list of objects to array of values
             $convertedData = [];
             foreach (get_object_vars($data) as $k => $v) {
-            
-            if ('_' !== substr($k, 0, 1) && 'w' !== $k) {
-$asUI = ('dt_' === substr($k ?? "",0,3))?(" :Formatted: ".formatDate($v)." : ".formatDateTime($v)):"";
-                $row = [];
-                $row[] = $k;
-                $row[] = $v . $asUI;
-                $row[] = $data->updateConvert($k, $v);
-$row[] = (empty($cached)?"---":$cached->{$k});
-
-$asUI = ('dt_' === substr($k ?? "",0,3))?(" :Formatted: ".formatDate($o->{$k})." : ".formatDateTime($o->{$k})):"";
-$row[] = (empty($o)?"---":$o->{$k}.$asUI);
-$row[] = (empty($result)?"---":$result[$k]);
-                $convertedData[] = $row;
-}
+                // skip any secret or nested properties!
+                if ('_' !== substr($k, 0, 1) && 'w' !== $k) {
+                    $row = [];
+                    $row[] = $k;
+                    // show as object, including typical UI reveal
+                    $asUI = ('dt_' === substr($k ?? "", 0, 3)) ? (" :Formatted: " . formatDate($v) . " : " . formatDateTime($v)) : "";
+                    $row[] = $v . $asUI;
+                    // show as would be pre-cast by insert-or-update:
+                    $row[] = $data->updateConvert($k, $v);
+                    // show as object-without-builder, including typical UI reveal
+                    $asUI = ('dt_' === substr($k ?? "", 0, 3)) ? (" :Formatted: " . formatDate($o->{$k}) . " : " . formatDateTime($o->{$k})) : "";
+                    $row[] = (empty($o) ? "---" : $o->{$k} . $asUI);
+                    // show as PDO select without any overrides (skip builder)
+                    $row[] = (empty($result) ? "---" : $result[$k]);
+                    $convertedData[] = $row;
+                }
             }
-            $results[] = new InsightReportInterface('Audit Report', ['All Properties','GetObject','PutAsUpdateConvert','Cached','FillAfterBypassedBuildSelect','RawFromBypassedBuildSelect'], $convertedData);
+            $results[] = new InsightReportInterface('Audit Report', ['All Properties', 'GetObject', 'WillPutAsUpdateConvert', 'FillWithoutBuildSelect', 'RawWithoutBuildSelect'], $convertedData);
         }
         return $results;
     }
