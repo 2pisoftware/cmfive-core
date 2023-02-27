a54180085711b0900de29315bb9839d388b3f54a
D.Crannaford <derek@2pisoftware.com>
2023-02-27 16:59:43 +1100

    First cut at arbitray count and catch

--- system/modules/file/actions/admin/moveToAdapter.php
+++ system/modules/file/actions/admin/moveToAdapter.php
@@ -4,6 +4,11 @@ function moveToAdapter_GET(Web $w)
 {
     $from_adapter = Request::string('from_adapter');
     $to_adapter = Request::string('to_adapter');
+    $max_count = Request::string('max_count');
+
+    if (empty($max_count)) {
+        $max_count = 1500;
+    }
 
     if (!empty(Config::get('file.adapters.' . $to_adapter)) && Config::get('file.adapters.' . $to_adapter . '.active') === true) {
         if (empty(Config::get('file.adapters.' . $from_adapter))) {
@@ -12,14 +17,27 @@ function moveToAdapter_GET(Web $w)
 
         // From index
         $count = 0;
+        $skipped = 0;
         $attachments = FileService::getInstance($w)->getAttachmentsForAdapter($from_adapter);
         if (!empty($attachments)) {
             foreach ($attachments as $attachment) {
-                $attachment->moveToAdapter($to_adapter);
-                $count++;
+                try {
+                    $attachment->moveToAdapter($to_adapter);
+                    $count++;
+                } catch (Exception $e) {
+                    LogService::getInstance($w)->error($e->getMessage());
+                    $skipped++;
+                }
+                if ($count > $max_count) {
+                    break;
+                }
             }
         }
-        $w->msg($count . ' attachment' . ($count == 1 ? '' : 's') . ' moved from "' . $from_adapter . '" to "' . $to_adapter . '"', '/file-admin');
+        $message = ($count . ' attachment' . ($count == 1 ? '' : 's') . ' moved from "' . $from_adapter . '" to "' . $to_adapter . '"');
+        if ($skipped > 0) {
+            $message = $message . ('<br>' . $skipped . ' attachment' . ($skipped == 1 ? '' : 's') . ' skipped, check logs.');
+        }
+        $w->msg($message, '/file-admin');
     } else {
         $w->error('Target adapter "' . $to_adapter . '" is either not found or not active', '/file-admin');
     }
