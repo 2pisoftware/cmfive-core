53033fe040bcac1ec5fd90c121a4670b27261626
DerekCrannaford <43056286+DerekCrannaford@users.noreply.github.com>
2023-04-04 05:00:54 +0000

    Patched task test

--- system/modules/task/tests/acceptance/helpers/CmfiveTaskModule.php
+++ system/modules/task/tests/acceptance/helpers/CmfiveTaskModule.php
@@ -231,7 +231,7 @@ class CmfiveTaskModule extends \Codeception\Module
      */
     public function deleteTask($I, $url)
     {
-        $I->amOnURL($url);
+        $I->amOnPage($url);
         $I->click('Delete');
         $I->acceptPopup();
     }
