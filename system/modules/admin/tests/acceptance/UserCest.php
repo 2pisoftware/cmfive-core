80a1d64a00b094a849fe95ce7f01c05537370a6c
2piJareem <122758853+2piJareem@users.noreply.github.com>
2023-06-01 03:23:18 +0000

    Took into account additional column on User List

--- system/modules/admin/tests/acceptance/UserCest.php
+++ system/modules/admin/tests/acceptance/UserCest.php
@@ -37,7 +37,7 @@ class UserCest
         $I->editUserGroupPermissions($I, 'Test User Group', ['user', 'comment']);
         $I->clickCmfiveNavbar($I, 'Admin', 'List Users');
         $row = $I->findTableRowMatching(1, $this->username);
-        $I->click('Permissions', ".table-responsive tbody tr:nth-child({$row}) td:nth-child(7)");
+        $I->click('Permissions', ".table-responsive tbody tr:nth-child({$row}) td:nth-child(8)");
         $I->seeCheckboxIsChecked('#check_comment');
         $disabledStatus = $I->grabAttributeFrom('#check_comment', 'disabled');
         $I->assertEquals($disabledStatus, 'true');
@@ -45,7 +45,7 @@ class UserCest
         $I->deleteUserGroup($I, 'Test User Group');
         $I->clickCmfiveNavbar($I, 'Admin', 'List Users');
         $row = $I->findTableRowMatching(1, $this->username);
-        $I->click('Permissions', ".table-responsive tbody tr:nth-child({$row}) td:nth-child(7)");
+        $I->click('Permissions', ".table-responsive tbody tr:nth-child({$row}) td:nth-child(8)");
         $I->dontSeeCheckboxIsChecked('#check_comment');
         $disabledStatus = $I->grabAttributeFrom('#check_comment', 'disabled');
         $I->assertEquals($disabledStatus, null);
