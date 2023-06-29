b2c68e262c1fdcbe09bdf65f8058b0330c4bb7e0
2piJareem <122758853+2piJareem@users.noreply.github.com>
2023-06-29 07:10:17 +0000

    Use bootstrap5 object

--- system/modules/insights/templates/manageMembers.tpl.php
+++ system/modules/insights/templates/manageMembers.tpl.php
@@ -1,5 +1,5 @@
 <?php 
 $insight_class = Request::string('insight_class');
 
-echo Html::box("/insights-members/editMembers?insight_class=" . $insight_class, "Add new member", true);
+echo HtmlBootstrap5::box("/insights-members/editMembers?insight_class=" . $insight_class, "Add new member", true, false, null, null, 'isbox', null, 'btn btn-sm btn-primary');
 echo $membersTable;
