4d80ac49435f4e236121f2ae8be48a8d58200473
claire <102092516+dragonflyfree@users.noreply.github.com>
2023-09-14 04:19:49 +0000

    add styling to lookup filter select

--- system/modules/admin/templates/lookup.tpl.php
+++ system/modules/admin/templates/lookup.tpl.php
@@ -6,7 +6,7 @@
     <div class='tab-body'>
         <div id='tab-1'>
             <?php echo HtmlBootstrap5::filter("Search Lookup Items", array(
-                array("Type", "select", "type", Request::mixed("types"), LookupService::getInstance($w)->getLookupTypes())
+                array("Type", "select", "type", Request::mixed("types"), LookupService::getInstance($w)->getLookupTypes(), "form-select")
             ), "/admin/lookup"); ?>
             <?php echo $listitem; ?>
         </div>
@@ -17,5 +17,7 @@
 </div>
 <script>
     // script to auto submit the filter form when the "type" is changed
-    document.getElementById("type").addEventListener("change", ({ target }) => target.form.submit());
+    document.getElementById("type").addEventListener("change", ({
+        target
+    }) => target.form.submit());
 </script>
