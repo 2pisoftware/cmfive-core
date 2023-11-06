71e27a0da670f3a5639f77ffd6a937bc198de93a
claire <102092516+dragonflyfree@users.noreply.github.com>
2023-11-06 05:15:53 +0000

    handle invalid migration names

--- system/modules/admin/actions/migration/create.php
+++ system/modules/admin/actions/migration/create.php
@@ -31,7 +31,24 @@ function create_POST(Web $w) {
 	
 	$name = Request::string('name', 'migration');
 
-	$name = str_replace(' ', '', $name);
+	// split on spaces, periods, and dashes, and ensure camel case
+	$name = implode("", array_map('ucfirst', preg_split('/[\s\.\-]/', $name)));
+
+	// ensure name is a valid php class name (excluding php keyword restrictions)
+	// if there are invalid characters, underline them in the error message banner
+	// valid class name regex: /^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*$/
+	// see: https://www.php.net/manual/en/language.oop5.basic.php
+	if (!preg_match('/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*$/', $name)) {
+		$invalid_character_regex = '/(^[^a-zA-Z_\x80-\xff])|([^a-zA-Z0-9_\x80-\xff])/';
+
+		// Wrap all invalid characters with a span to apply the red squiggly underline class
+		$name = preg_replace_callback(
+			$invalid_character_regex, function ($matches) {
+			return '<span class="red-squiggly-underline">' . $matches[0] . '</span>';
+		}, $name);
+
+		$w->error("Invalid migration name: " . $name, "/admin-migration");
+	}
 
 	$response = MigrationService::getInstance($w)->createMigration($p['module'], $name);
 	
