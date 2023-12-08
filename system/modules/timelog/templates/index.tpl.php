47a4851074b274504f31aba38a7a3225ba2531d7
Dane Evans <dane@2pisoftware.com>
2023-12-08 14:44:39 +1100

    Add additioanl data into timelog page - Should mostly give a link back to projects

--- system/modules/timelog/templates/index.tpl.php
+++ system/modules/timelog/templates/index.tpl.php
@@ -3,12 +3,60 @@
         <?php foreach($time_entries as $date => $entry_struct) : ?>
             <h4 style='border-bottom: 1px solid #777;'><?php echo $date; ?><span class='right'><?php echo TaskService::getInstance($w)->getFormatPeriod($entry_struct['total']); ?></span></h4>
             <table class='small-12'>
-                <thead><tr><th width="10%">From</th><th width="10%">To</th><th width="20%">Object</th><th width="40%">Description</th><th>Actions</th></tr></thead>
+                <thead><tr><th width="10%">From</th><th width="10%">To</th><th width="10%">Additionals</th><th width="20%">Object</th><th width="40%">Description</th><th>Actions</th></tr></thead>
                 <tbody>
                     <?php foreach($entry_struct['entries'] as $time_entry) : ?>
                         <tr>
                             <td><?php echo formatDate($time_entry->dt_start, "H:i:s"); ?></td>
                             <td><?php echo formatDate($time_entry->dt_end, "H:i:s"); ?></td>
+
+                            <?php
+                        // Call hook and filter out empty/false values
+                        if (!empty($task->id)) : ?>
+                            <div class='row-fluid panel clearfix' id='task_subscribers'>
+                                <table class="small-12 columns">
+                                    <tbody>
+                                        <tr>
+                                            <td class="section" colspan="1">Subscribers <br> <?php echo Html::box('/task-subscriber/add/' . $task->id, 'Add', true, false, null, null, 'isbox', null, 'info center'); ?></td>
+                                        </tr>
+                                        <?php if (!empty($subscribers)) : ?>
+                                            <?php foreach ($subscribers as $subscriber) : ?>
+                                                <?php $subscriber_user = $subscriber->getUser(); ?>
+                                                <?php if (!empty($subscriber_user)) : ?>
+                                                    <tr <?php echo ($subscriber_user->is_external) ? 'style="background-color: #c99;"' : ''; ?>>
+                                                        <td><?php echo $subscriber_user->getFullName(); ?> - <?php echo $subscriber_user->getContact()->email; ?></br>
+                                                            <?php echo Html::b('/task-subscriber/delete/' . $subscriber->id, 'Delete', 'Are you sure you want to remove this subscriber?', null, false, 'warning center'); ?></td>
+                                                    </tr>
+                                                <?php endif; ?>
+                                            <?php endforeach; ?>
+                                        <?php endif; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+
+
+                            <?php $additional_details = $w->callHook('task', 'additional_details', $task);
+                            if (!is_null($additional_details) && is_array($additional_details)) {
+                                $additional_details_flattened = [];
+                                foreach ($additional_details as $module_details) {
+                                    if (isset($module_details[0]) && !is_array($module_details[0])) {
+                                        $additional_details_flattened[] = $module_details;
+                                    } else {
+                                        foreach ($module_details as $details) {
+                                            $additional_details_flattened[] = $details;
+                                        }
+                                    }
+                                }
+                                if (!empty($additional_details_flattened)) : ?>
+                                    <tr>
+                                    <?php foreach ($additional_details_flattened as $additional_detail) : ?>
+                                            <td><?php echo $additional_detail[0] . ':' . $additional_detail[1] ; ?></td>
+                                    <?php endforeach; ?>
+                                    </tr>
+                                <?php endif;
+                            }
+                        endif;
+                        ?>
                             <td><?php echo($time_entry->getLinkedObject() ? get_class($time_entry->getLinkedObject()) . ": " . $time_entry->getLinkedObject()->toLink() : ''); ?></td>
                             <td><pre class="break-pre" style="font-family: sans-serif;"><?php echo $time_entry->getComment()->comment; ?></pre></td>
                             <td>
