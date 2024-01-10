38fd82e217b73c5119eb6d6f30cd6357a5b411c6
Tynan Matthews <30655787+tynanmatthews@users.noreply.github.com>
2024-01-10 03:51:05 +0000

    Refactored action and template for timelog index to move logic to action

--- system/modules/timelog/actions/index.php
+++ system/modules/timelog/actions/index.php
@@ -29,5 +29,48 @@ function index_GET(Web $w)
         }
     }
 
-    $w->ctx('time_entries', $time_entry_objects);
+    //FIXME build tables
+    $time_entries = $time_entry_objects;
+    $timelog_table = '';
+    if (!empty($time_entries)) {
+        foreach ($time_entries as $date => $entry_struct) {
+            $timelog_table .= '<div style="padding-bottom: 20px;">';
+            $timelog_table .= '<h4 style="border-bottom: 1px solid #777;">' . $date . '<span style="float: right;">' . TaskService::getInstance($w)->getFormatPeriod($entry_struct['total']) . '</span></h4>';
+            $timelog_table .= '<table class="table-striped"><thead><tr>';
+            //TODO automate this based on if there are additional rows
+            $timelog_table .= '<th width="' . "5" . '%">' . 'From' . '</th>';
+            $timelog_table .= '<th width="' . "5" . '%">' . 'To' . '</th>';
+            $timelog_table .= '<th width="' . "10" . '%">' . 'Project' . '</th>';
+            $timelog_table .= '<th width="' . "15" . '%">' . 'Task' . '</th>';
+            $timelog_table .= '<th width="' . "25" . '%">' . 'Description' . '</th>';
+            $timelog_table .= '<th width="' . "40" . '%">' . 'Actions' . '</th>';
+
+            $timelog_table .= '</tr></thead><tbody>';
+            foreach ($entry_struct['entries'] as $time_entry) {
+                $timelog_table .= '<tr>';
+                $timelog_table .= '<td>' . formatDate($time_entry->dt_start, "H:i:s") . '</td>';
+                $timelog_table .= '<td>' . formatDate($time_entry->dt_end, "H:i:s") . '</td>';
+                $timelog_table .= '<td>' . 'Misc proj' . '</td>';
+                $timelog_table .= '<td>' . ($time_entry->getLinkedObject() ? (get_class($time_entry->getLinkedObject()) . ": " . $time_entry->getLinkedObject()->toLink()) : '') . '</td>';
+                $timelog_table .= '<td>' . ($time_entry->getComment() ? $time_entry->getComment()->comment : 'no comment found') . '</td>';
+                $timelog_table .= '<td>' . HtmlBootstrap5::buttonGroup(($time_entry->object_class == 'Task' ? HtmlBootstrap5::b('/task/edit/' . $time_entry->object_id . "#timelog", "View All", null, null, false, "btn btn-primary") : '') .
+                        ($time_entry->canEdit(AuthService::getInstance($w)->user()) ? HtmlBootstrap5::box('/timelog/edit/' . $time_entry->id, "Edit", true, null, null, null, "isbox", null, "btn btn-secondary") : '') .
+                        ($time_entry->canEdit(AuthService::getInstance($w)->user()) || $time_entry->canDelete(AuthService::getInstance($w)->user()) ? HtmlBootstrap5::dropdownButton(
+                            "More",
+                            [
+                                ($time_entry->canEdit(AuthService::getInstance($w)->user()) ? HtmlBootstrap5::box('/timelog/move/' . $time_entry->id, 'Move', true, null, null, null, "isbox", null, "dropdown-item btn-sm text-start") : ''),
+                                '<hr class="dropdown-divider">',
+                                ($time_entry->canDelete(AuthService::getInstance($w)->user()) ? HtmlBootstrap5::b('/timelog/delete/' . $time_entry->id, 'Delete', empty($confirmation_message) ? 'Are you sure you want to delete this timelog?' : $confirmation_message, null, false, "dropdown-item btn-sm text-start") : '')
+                            ],
+                            "btn-info btn btn-sm rounded-0 rounded-end-1"
+                        ) : '')
+                ) . '</td>';
+                $timelog_table .= '</tr>';
+            }
+            $timelog_table .= '</tbody></table></div>';
+        }
+    } else {
+        $timelog_table = '<h4>No time logs found</h4>';
+    }
+    $w->ctx('tables', $timelog_table);
 }
--- system/modules/timelog/templates/index.tpl.php
+++ system/modules/timelog/templates/index.tpl.php
@@ -1,51 +1,5 @@
 <div class='row-fluid'>
-    <?php if (!empty($time_entries)) : ?>
-        <?php foreach ($time_entries as $date => $entry_struct) : ?>
-            <div style="padding-bottom: 20px;">
-                <h4 style='border-bottom: 1px solid #777;'><?php echo $date ?> <span style='float: right;'><?php echo TaskService::getInstance($w)->getFormatPeriod($entry_struct['total']) ?></span></h4>
-                <table class="table-striped">
-                    <thead>
-                        <tr>
-                            <th class=" column-time">From</th>
-                            <th class="column-time">To</th>
-                            <th class="column-object">Task</th>
-                            <th class="column-description">Description</th>
-                            <th class="column-actions">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($entry_struct['entries'] as $time_entry) : ?>
-                            <tr>
-                                <td><?php echo formatDate($time_entry->dt_start, "H:i:s") ?></td>
-                                <td><?php echo formatDate($time_entry->dt_end, "H:i:s") ?></td>
-                                <td><?php echo $time_entry->getLinkedObject() ? get_class($time_entry->getLinkedObject()) . ": " . $time_entry->getLinkedObject()->toLink() : '' ?></td>
-                                <td><?php echo $time_entry->getComment()->comment ?></td>
-                                <td>
-
-                                    <?php
-                                    echo HtmlBootstrap5::buttonGroup(
-                                        ($time_entry->object_class == 'Task' ? HtmlBootstrap5::b('/task/edit/' . $time_entry->object_id . "#timelog", "View All", null, null, false, "btn btn-primary") : '') .
-                                            ($time_entry->canEdit(AuthService::getInstance($w)->user()) ? HtmlBootstrap5::box('/timelog/edit/' . $time_entry->id, "Edit", true, null, null, null, "isbox", null, "btn btn-secondary") : '') .
-                                            ($time_entry->canEdit(AuthService::getInstance($w)->user()) || $time_entry->canDelete(AuthService::getInstance($w)->user()) ? HtmlBootstrap5::dropdownButton(
-                                                "More",
-                                                [
-                                                    ($time_entry->canEdit(AuthService::getInstance($w)->user()) ? HtmlBootstrap5::box('/timelog/move/' . $time_entry->id, 'Move', true, null, null, null, "isbox", null, "dropdown-item btn-sm text-start") : ''),
-                                                    '<hr class="dropdown-divider">',
-                                                    ($time_entry->canDelete(AuthService::getInstance($w)->user()) ? HtmlBootstrap5::b('/timelog/delete/' . $time_entry->id, 'Delete', empty($confirmation_message) ? 'Are you sure you want to delete this timelog?' : $confirmation_message, null, false, "dropdown-item btn-sm text-start") : '')
-                                                ],
-                                                "btn-info btn btn-sm rounded-0 rounded-end-1"
-                                            ) : '')
-                                    ); ?>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-        <?php endforeach; ?>
-    <?php else : ?>
-        <h4>No time logs found</h4>
-    <?php endif; ?>
+    <?php echo $tables ?>
 </div>
 
 <?php if (!empty($pagination)) : ?>
