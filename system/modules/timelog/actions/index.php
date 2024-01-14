8abcf0c1f113a0e83b7a69529fc7d68bb08ed5c3
Tynan Matthews <30655787+tynanmatthews@users.noreply.github.com>
2024-01-14 21:52:48 +0000

    Add dynamic table headers and column widths to timelog table

--- system/modules/timelog/actions/index.php
+++ system/modules/timelog/actions/index.php
@@ -30,27 +30,36 @@ function index_GET(Web $w)
     }
 
     //FIXME build tables
+
+    $timelog_table_headers = ['From', 'To', 'Task', 'Description', 'Actions'];
+    $timelog_table_column_widths = [10, 10, 24, 40, 16];
+
     $time_entries = $time_entry_objects;
+    // Cannot use inbuilt table functions as do not allow for fixed width columns
     $timelog_table = '';
     if (!empty($time_entries)) {
         foreach ($time_entries as $date => $entry_struct) {
+            // Build table for each day, including headers
             $timelog_table .= '<div style="padding-bottom: 20px;">';
             $timelog_table .= '<h4 style="border-bottom: 1px solid #777;">' . $date . '<span style="float: right;">' . TaskService::getInstance($w)->getFormatPeriod($entry_struct['total']) . '</span></h4>';
             $timelog_table .= '<table class="table-striped"><thead><tr>';
-            //TODO automate this based on if there are additional rows
-            $timelog_table .= '<th width="' . "5" . '%">' . 'From' . '</th>';
-            $timelog_table .= '<th width="' . "5" . '%">' . 'To' . '</th>';
-            $timelog_table .= '<th width="' . "10" . '%">' . 'Project' . '</th>';
-            $timelog_table .= '<th width="' . "15" . '%">' . 'Task' . '</th>';
-            $timelog_table .= '<th width="' . "25" . '%">' . 'Description' . '</th>';
-            $timelog_table .= '<th width="' . "40" . '%">' . 'Actions' . '</th>';
 
+            //not every day will have the same number of columns depending on additional info from external modules
+            for ($index = 0; $index < count($timelog_table_headers); $index++) {
+                $timelog_table .= '<th width="' . $timelog_table_column_widths[$index] . '%">' . $timelog_table_headers[$index] . '</th>';
+            }
             $timelog_table .= '</tr></thead><tbody>';
+
             foreach ($entry_struct['entries'] as $time_entry) {
+                //load, if it exists, additional details
+                $hook_response = $w->callHook('task', 'additional_details', $time_entry->getLinkedObject());
+                $additional_details = $hook_response[0];
+                $project = !empty($additional_details[0]) ? $additional_details[0][1] : 'No project assigned';
+
                 $timelog_table .= '<tr>';
                 $timelog_table .= '<td>' . formatDate($time_entry->dt_start, "H:i:s") . '</td>';
                 $timelog_table .= '<td>' . formatDate($time_entry->dt_end, "H:i:s") . '</td>';
-                $timelog_table .= '<td>' . 'Misc proj' . '</td>';
+                $timelog_table .= '<td>' . $additional_details . '</td>';
                 $timelog_table .= '<td>' . ($time_entry->getLinkedObject() ? (get_class($time_entry->getLinkedObject()) . ": " . $time_entry->getLinkedObject()->toLink()) : '') . '</td>';
                 $timelog_table .= '<td>' . ($time_entry->getComment() ? $time_entry->getComment()->comment : 'no comment found') . '</td>';
                 $timelog_table .= '<td>' . HtmlBootstrap5::buttonGroup(($time_entry->object_class == 'Task' ? HtmlBootstrap5::b('/task/edit/' . $time_entry->object_id . "#timelog", "View All", null, null, false, "btn btn-primary") : '') .
