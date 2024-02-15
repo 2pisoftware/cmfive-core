0c6e73cc87d93dd4a86cfc01402aa38846b61399
Tynan Matthews <30655787+tynanmatthews@users.noreply.github.com>
2024-02-15 01:49:10 +0000

    updated layout for auth module templates to use BS5

--- system/modules/auth/templates/login.tpl.php
+++ system/modules/auth/templates/login.tpl.php
@@ -6,29 +6,38 @@
                 "name" => CSRF::getTokenID(),
                 "value" => CSRF::getTokenValue(),
             ])) .
-            (new \Html\Form\InputField\Hidden([
-                "name" => "login",
-            ]))->setAttribute("v-model", "login") .
-            (new \Html\Form\InputField\Hidden([
-                "name" => "password",
-            ]))->setAttribute("v-model", "password");
+                (new \Html\Form\InputField\Hidden([
+                    "name" => "login",
+                ]))->setAttribute("v-model", "login") .
+                (new \Html\Form\InputField\Hidden([
+                    "name" => "password",
+                ]))->setAttribute("v-model", "password");
             ?>
-            <label>MFA Code
-                <?php
-                echo (new \Html\Form\InputField([
-                    "id|name" => "mfa_code",
-                    "placeholder" => "Your code",
-                    "required" => true,
-                ]))->setAttribute("v-model", "mfa_code");
-                ?>
-            </label>
-            <button type="submit" class="button medium-5 small-12">Confirm</button>
-            <button class="button info medium-5 small-12 right" @click.prevent="back">Back</button>
+            <div class='row'>
+                <label for='mfa_code' class='col-form-label'>MFA Code
+                    <?php
+                    echo (new \Html\Form\InputField([
+                        "id|name" => "mfa_code",
+                        "placeholder" => "Your code",
+                        "required" => true,
+                        "class" => "form-control-lg",
+                    ]))->setAttribute("v-model", "mfa_code");
+                    ?>
+                </label>
+            </div>
+            <div class='row d-flex justify-content-between mt-3'>
+                <div class='col'>
+                    <button type="submit" class="btn btn-primary w-100">Confirm</button>
+                </div>
+                <div class='col'>
+                    <button class="btn btn-secondary w-100" @click.prevent="back">Back</button>
+                </div>
+            </div>
         </form>
     </div>
     <div v-else>
         <form @submit.prevent="executeLogin">
-            <div data-alert class="alert-box alert" v-if="error_message != null">
+            <div data-alert class="alert-box alert row" v-if="error_message != null">
                 {{ error_message }}
                 <a href="#" class="close" @click="error_message = null">&times;</a>
             </div>
@@ -38,26 +47,36 @@
                 "value" => CSRF::getTokenValue(),
             ]));
             ?>
-            <label><?php echo Config::get('auth.login_label', 'Login'); ?>
-                <?php
-                echo (new \Html\Form\InputField([
-                    "id|name" => "login",
-                    "placeholder" => Config::get('auth.login_label', 'Login'),
-                    "required" => true,
-                ]))->setAttribute("v-model", "login");
-                ?>
-            </label>
-            <label>Password
-                <?php
-                echo (new \Html\Form\InputField\Password([
-                    "id|name" => "password",
-                    "placeholder" => "Your password",
-                    "required" => true,
-                ]))->setAttribute("v-model", "password");
-                ?>
-            </label>
-            <button type="submit" class="button medium-5 small-12">Login</button>
-            <a onclick="window.location.href='/auth/forgotpassword';" class="medium-5 small-12 right text-right"><?php echo $passwordHelp; ?></a>
+            <div class='row'>
+                <label for='login' class='col-form-label'><?php echo Config::get('auth.login_label', 'Login'); ?>
+                    <?php
+                    echo (new \Html\Form\InputField([
+                        "id|name" => "login",
+                        "placeholder" => Config::get('auth.login_label', 'Login'),
+                        "required" => true,
+                        "class" => "form-control-lg",
+                    ]))->setAttribute("v-model", "login");
+                    ?>
+                </label>
+                <label for='password' class='col-form-label'>Password
+                    <?php
+                    echo (new \Html\Form\InputField\Password([
+                        "id|name" => "password",
+                        "placeholder" => "Your password",
+                        "required" => true,
+                        "class" => "form-control-lg",
+                    ]))->setAttribute("v-model", "password");
+                    ?>
+                </label>
+            </div>
+            <div class='row d-flex justify-content-between mt-3'>
+                <div class='col'>
+                    <button type="submit" class="btn btn-primary w-100 h-auto">Login</button>
+                </div>
+                <div class='col text-end'>
+                    <a onclick="window.location.href='/auth/forgotpassword';" class="btn w-auto text-end"><?php echo $passwordHelp; ?></a>
+                </div>
+            </div>
         </form>
     </div>
 </div>
@@ -103,9 +122,9 @@
                     }
                 }).catch(function(error) {
                     _this.login = null,
-                    _this.password = null,
-                    _this.mfa_code = null,
-                    _this.is_mfa_enabled = false;
+                        _this.password = null,
+                        _this.mfa_code = null,
+                        _this.is_mfa_enabled = false;
                     _this.error_message = error.response.data;
                 }).finally(function() {
                     _this.is_loading = false;
