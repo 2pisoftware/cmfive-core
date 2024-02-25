bc5a62a3a37290e52f4632ee94e2b66667758597
Tynan Matthews <30655787+tynanmatthews@users.noreply.github.com>
2024-02-25 23:07:18 +0000

    fix issue causing template tests to fail

--- system/modules/admin/actions/templates/edit.php
+++ system/modules/admin/actions/templates/edit.php
@@ -27,7 +27,7 @@ function edit_GET(Web $w)
 				'label' => 'Active',
 				"class" => "",
 				"checked" => $t->is_active ? $t->is_active : null
-			]))// ->setAttribute("is_active", $t->is_active) //["Active", "checkbox", "is_active", $t->is_active]
+			])) // ->setAttribute("is_active", $t->is_active) //["Active", "checkbox", "is_active", $t->is_active]
 		],
 		[
 			(new \Html\Form\InputField\Text([
@@ -71,7 +71,7 @@ function edit_GET(Web $w)
 			(new \Html\Cmfive\CodeMirrorEditor([
 				"id|name" => "template_body",
 				"value" => $t->template_body,
-			]))//->addToConfig(['extensions' => ['basicSetup'], 'parent' => 'template_body']) //["", "textarea", "template_body", $t->template_body, 60, 100, "codemirror"]
+			])) //->addToConfig(['extensions' => ['basicSetup'], 'parent' => 'template_body']) //["", "textarea", "template_body", $t->template_body, 60, 100, "codemirror"]
 		]
 	];
 
@@ -85,7 +85,7 @@ function edit_GET(Web $w)
 				"value" => $t->test_title_json,
 				"maxlength" => 500
 			])) //["", "textarea", "test_title_json", $t->test_title_json, 100, 5, false]]
-		] 
+		]
 	];
 	$newForm["Body Data"] = [
 		[
@@ -94,7 +94,7 @@ function edit_GET(Web $w)
 				"value" => $t->test_body_json,
 				"maxlength" => 2000
 			])) // ["", "textarea", "test_body_json", $t->test_body_json, 100, 20, false]]
-		] 
+		]
 	];
 
 	$w->ctx("testdataform", HtmlBootstrap5::multiColForm($newForm, $w->localUrl('/admin-templates/edit/' . $t->id)));
@@ -106,7 +106,8 @@ function edit_POST(Web $w)
 	$p = $w->pathMatch("id");
 	$t = $p["id"] ? TemplateService::getInstance($w)->getTemplate($p['id']) : new Template($w);
 	$t->fill($_POST);
-    $t->template_body = json_decode($_POST['template_body']);
+	$encoded_template_body = $_POST['template_body'] ?? '';
+	$t->template_body = json_decode($encoded_template_body);
 
 	// Set is active if saving is originating from the first page
 	if (isset($_POST["title"]) && isset($_POST["module"]) && isset($_POST["category"])) {
