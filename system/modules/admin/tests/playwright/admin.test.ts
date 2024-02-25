e6e283ee341dceb397340d9eb2c9f5992bfee0bb
Tynan Matthews <30655787+tynanmatthews@users.noreply.github.com>
2024-02-25 22:34:32 +0000

    remove redundant file

--- system/modules/admin/tests/playwright/admin.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { PlaywrightHelper } from './helpers';
-
-test('test admin page', async ({ page }) => {
-    await PlaywrightHelper.login(page, 'admin', 'admin');
-    
-});
