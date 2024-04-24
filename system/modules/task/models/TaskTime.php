40b80ea83d2b59a9a1f80471a9e64c4bf7af333c
timl07 <165957036+timl07@users.noreply.github.com>
2024-04-24 11:10:00 +1000

    Writing methods to get time for a task by time type and whether billable

--- system/modules/task/models/TaskTime.php
+++ system/modules/task/models/TaskTime.php
@@ -28,5 +28,41 @@ class TaskTime extends  DbObject {
     public function getTask() {
         return $this->getObject("Task", $this->task_id);
     }
-    
+
+    public function getTotalTimeByTimeType() {
+        $all_tasks = TaskService::getInstance($this->w)->getTasks();
+        foreach ($all_tasks as $task) {
+            if ($task->id == $this->task_id) {
+                $current_task = $task;
+            }
+        }
+        $all_timelogs_for_task = TimelogService::getInstance($this->w)->getTimelogsForObject($current_task);
+        
+        $timelog_types = Config::get('task.TaskType_' . $current_task->task_type)['time-types'];
+        $time_totals_for_time_types = [];
+        foreach ($timelog_types as $timelog_type) {
+            $total_time_for_type = 0;
+            foreach ($all_timelogs_for_task as $timelog) {
+                if ($timelog->time_type == $timelog_type) {
+                    $total_time_for_type += $timelog->getDuration();
+                }
+                $total_time_fmtd = floor($total_time_for_type / 3600) . gmdate(":i:s", $total_time_for_type) . '  ';
+            }
+            $time_totals_for_time_types[$timelog_type] = $total_time_fmtd;
+        }
+        return $time_totals_for_time_types;
+    }
+
+    public function getTotalTimeByBillable() {
+        $time_totals = $this->getTotalTimeByTimeType();
+        $time_totals_in_seconds = [];
+        foreach ($time_totals as $time_total) {
+            $time_totals_in_seconds[] = strtotime("1970-01-01 $time_total UTC");
+        }
+        $billable_time_in_seconds = array_sum(array_merge(array_slice($time_totals_in_seconds, 0, 3), [$time_totals_in_seconds[4]]));
+        $billable_time = floor($billable_time_in_seconds / 3600) . gmdate(":i:s", $billable_time_in_seconds);
+        $nonbillable_time = floor($time_totals_in_seconds[3] / 3600) . gmdate(":i:s", $time_totals_in_seconds[3]);
+        return ['Billable' => $billable_time, 'Non-Billable' => $nonbillable_time];
+    }
+
 }
