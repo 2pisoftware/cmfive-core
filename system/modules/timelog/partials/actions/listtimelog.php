8ee0b9aab546c5adf82ca1ffd672edfebe7631b3
timl07 <165957036+timl07@users.noreply.github.com>
2024-04-24 11:02:51 +1000

    Displaying billable hours above timelog list

--- system/modules/timelog/partials/actions/listtimelog.php
+++ system/modules/timelog/partials/actions/listtimelog.php
@@ -11,10 +11,24 @@ function listtimelog(\Web $w, $params) {
 			return $carry += $timelog->getDuration();
 		});
 	}
+
+	$all_tasks = \TaskService::getInstance($w)->getTasks();
+	foreach ($all_tasks as $task) {
+		if ($task->id == $params['object_id']) {
+			$current_task = $task;
+		}
+	}
+	if (in_array($current_task->task_type, ['Todo', 'CmfiveTicket', 'CrmTicket']) && sizeof($timelogs) > 0) {
+		$task_time = \TaskTime::getInstance($w);
+		$task_time->task_id = $params['object_id'];
+	} else {
+		$task_time = null;
+	}
 	
 	$w->ctx("total", !empty($total) ? $total : 0);
 	$w->ctx("class", $params['object_class']);
 	$w->ctx("id", $params['object_id']);
 	$w->ctx("redirect", !empty($params['redirect']) ? $params['redirect'] : "");
 	$w->ctx("timelogs", $timelogs);
+	$w->ctx("billable_hours", !empty($task_time) ? $task_time->getTotalTimeByBillable() : null);
 }
