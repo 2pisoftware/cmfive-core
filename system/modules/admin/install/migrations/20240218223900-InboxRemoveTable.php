2f0bd2fb5c08755bfad7948bfe693e33243681f4
Derek Crannaford <derek@2pisoftware.com>
2024-09-11 11:32:35 +1000

    lint/layout on changes only

--- system/modules/admin/install/migrations/20240218223900-InboxRemoveTable.php
+++ system/modules/admin/install/migrations/20240218223900-InboxRemoveTable.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Removes the inbox table from the Inbox module that is no longer used or supported.
  */
@@ -12,17 +13,16 @@ class InboxRemoveTable extends CmfiveMigration
         $this->dropTable('inbox_message');
 
         //Clear out the related user roles
-		if ($this->hasTable('user_role')) {
+        if ($this->hasTable('user_role')) {
             $this->w->db->delete('user_role')->where('role', 'inbox_sender')->execute();
             $this->w->db->delete('user_role')->where('role', 'inbox_reader')->execute();
         }
     }
 
-    public function down(): void
-    {
-    }
+    public function down(): void {}
 
-    public function description() {
+    public function description()
+    {
         return 'Remove inbox table and roles';
     }
 }
