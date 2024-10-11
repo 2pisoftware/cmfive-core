870a70d17490cb14962344429b53b7c11eb78ef0
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2024-10-11 05:45:24 +0000

    fix quill not mounting to modals

--- system/templates/base/src/js/components/QuillEditor.ts
+++ system/templates/base/src/js/components/QuillEditor.ts
@@ -9,14 +9,14 @@ export class QuillEditor {
 
     static bindQuillEditor()
     {
-        const quillEditors = document.querySelectorAll(QuillEditor.quillTarget);
+        const quillEditors: NodeListOf<HTMLElement> = document.querySelectorAll(QuillEditor.quillTarget);
 
         if (quillEditors) {
             quillEditors.forEach((q) => {
 				if (q.classList.contains(QuillEditor.quillExistsTarget)) return;
 
                 const options = q.getAttribute('data-quill-options');
-                let editor = new Quill('#' + q.id, JSON.parse(options));
+                let editor = new Quill(q, JSON.parse(options));
 
                 const textarea = document.getElementById(q.id.substring(6));
                 q.closest('form').removeEventListener('submit', () => textarea.innerText = q.querySelector('.ql-editor').innerHTML);
