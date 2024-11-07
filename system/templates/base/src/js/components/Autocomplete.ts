2pisoftware/cmfive-core

commit f1fb50dce99bd8bcc59f7619bfbf59b024c124fd

fix: tomselect clear input when selecting option

system/templates/base/src/js/components/Autocomplete.ts
@@ -16,8 +16,22 @@ export class Autocomplete {
             }
 
 			// tbh I would prefer a save button, but the cmfive api is already set up for add, remove, create methods
-			if (parsed?.cmfive?.onItemAdd)
-				parsed.onItemAdd = Autocomplete.tomSelectChange.bind(null, new URL(parsed.cmfive.onItemAdd));
+			// (wrt the tags module)
+
+			if (parsed?.cmfive?.onItemAdd) {
+				parsed.onItemAdd = function() {
+					this.setTextboxValue("");
+					this.refreshOptions();
+					Autocomplete.tomSelectChange.call(null, new URL(parsed.cmfive.onItemAdd))
+				};
+			}
+			else {
+				parsed.onItemAdd = function() {
+					this.setTextboxValue("");
+					this.refreshOptions();
+				}
+			}
+
 			if (parsed?.cmfive?.onItemRemove)
 				parsed.onItemRemove = Autocomplete.tomSelectChange.bind(null, new URL(parsed.cmfive.onItemRemove));
 			if (parsed?.cmfive?.onItemCreate)
@@ -36,7 +50,6 @@ export class Autocomplete {
 		endpoint: URL,
 		value: string
 	) => {
-
 		endpoint.searchParams.set("value", value);
 
 		fetch(endpoint, {

commit 7358a0591756cbd635805f8368acddb5526fb524

fix: autocomplete not sending add events
I broke it in f1fb50dce99bd8bcc59f7619bfbf59b024c124fd

system/templates/base/src/js/components/Autocomplete.ts
@@ -19,10 +19,10 @@ export class Autocomplete {
 			// (wrt the tags module)
 
 			if (parsed?.cmfive?.onItemAdd) {
-				parsed.onItemAdd = function() {
+				parsed.onItemAdd = function(item: string) {
 					this.setTextboxValue("");
 					this.refreshOptions();
-					Autocomplete.tomSelectChange.call(null, new URL(parsed.cmfive.onItemAdd))
+					Autocomplete.tomSelectChange.call(null, new URL(parsed.cmfive.onItemAdd), item)
 				};
 			}
 			else {