48dca20938d62775debfa69d5e12b4427b63518a
Madeline Carlier <madeline@2pisoftware.com>
2025-04-22 02:26:07 +0000

    fix: text overflow in sidebar and dropdown menus

--- system/templates/layout-bootstrap-5.tpl.php
+++ system/templates/layout-bootstrap-5.tpl.php
@@ -100,9 +100,9 @@ $theme_setting = AuthService::getInstance($w)->getSettingByKey('bs5-theme');
                                         <ul class="nav flex-column">
                                             <?php foreach ($module_navigation as $module_nav) :
                                                 if (is_string($module_nav)) : ?>
-                                                    <li class="nav-item"><?php echo $module_nav; ?></li>
+                                                    <li class="nav-item w-100 text-truncate"><?php echo $module_nav; ?></li>
                                                 <?php else : ?>
-                                                    <li class="nav-item"><a <?php echo $module_nav->type == MenuLinkType::Modal ? 'data-modal-target' : 'href'; ?>="<?php echo $module_nav->url; ?>"><?php echo $module_nav->title; ?></a></li>
+                                                    <li class="nav-item w-100 text-truncate"><a <?php echo $module_nav->type == MenuLinkType::Modal ? 'data-modal-target' : 'href'; ?>="<?php echo $module_nav->url; ?>"><?php echo $module_nav->title; ?></a></li>
                                                 <?php endif;
                                             endforeach; ?>
                                         </ul>
@@ -212,7 +212,7 @@ $theme_setting = AuthService::getInstance($w)->getSettingByKey('bs5-theme');
                                                         }
                                                     }
                                                 } ?>
-                                                <div class="dropdown-menu" aria-labelledby="topnav_<?php echo $module; ?>_dropdown_link">
+                                                <div class="dropdown-menu cosine-header-dropdowns" aria-labelledby="topnav_<?php echo $module; ?>_dropdown_link">
                                                     <?php foreach ($module_navigation as $module_nav) :
                                                         if (is_string($module_nav)) {
                                                             echo $module_nav;
